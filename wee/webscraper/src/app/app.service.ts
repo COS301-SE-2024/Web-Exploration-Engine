9ea03dc5661d0464ef9f5c4ade6ab39953259168
Siyamthanda Ndlovu <99127918+siyamthandandlovu@users.noreply.github.com>
2024-05-27 18:43:05 +0200

    Update app.service.ts - testci

--- wee/webscraper/src/app/app.service.ts
+++ wee/webscraper/src/app/app.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  getData(): { message: stri
   }
 }
