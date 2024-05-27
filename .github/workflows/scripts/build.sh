f6ce06bdb35f91c1887416c7955cc190eb9a17ec
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-05-27 09:49:42 +0200

    workflows can run webscraper app

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -1,5 +1,6 @@
 #!/user/bin/env bash
 cd wee
+npx nx build webscraper
 npx nx build frontend
 
 echo "================== build complete =================="
--- .github/workflows/scripts/test.sh
+++ .github/workflows/scripts/test.sh
@@ -1,4 +1,6 @@
 #!/user/bin/env bash
 cd wee
+
+npx nx test webscraper
 npx nx test frontend
 echo "================== test complete =================="
