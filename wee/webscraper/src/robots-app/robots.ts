bda6f873b959f360f45ffc848063e77c0abc302d
katnicole14 <u22543946@tuks.co.za>
2024-05-28 07:37:38 +0200

    robot.txt can now return allowed paths

--- wee/webscraper/src/robots-app/robots.ts
+++ wee/webscraper/src/robots-app/robots.ts
@@ -1,7 +1,9 @@
 import fetch from 'node-fetch';
 
-export async function extractAllowedPaths(baseUrl: string): Promise<Set<string>> {
-  const robotstxtUrl = `${baseUrl}/robots.txt`;
+export async function extractAllowedPaths(url: string): Promise<Set<string>> {
+
+  const domain = extractDomain(url);
+  const robotstxtUrl = `${domain}/robots.txt`;
 
   const response = await fetch(robotstxtUrl);
   const robotstxt = await response.text();
@@ -11,7 +13,11 @@ export async function extractAllowedPaths(baseUrl: string): Promise<Set<string>>
 
   lines.forEach((line) => {
     if (line.startsWith('Allow:')) {
-      const path = line.substring(6).trim();
+      let path = line.substring(6).trim();
+      if (path.includes('*')) {
+        // Remove everything after the asterisk (*) also handle this differently in the future
+        path = path.substring(0, path.indexOf('*'));
+    }
       if (path.startsWith('/')) {
         allowedPaths.add(path);
       }
@@ -24,4 +30,14 @@ export async function extractAllowedPaths(baseUrl: string): Promise<Set<string>>
   });
 
   return allowedPaths;
+}
+
+function extractDomain(url: string): string {
+  try {
+      const parsedUrl = new URL(url);
+      return parsedUrl.origin;
+  } catch (error) {
+      console.error('Invalid URL:', error);
+      throw new Error('Invalid URL');
+  }
 }
