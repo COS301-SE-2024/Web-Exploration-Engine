624c3351fbd56b0c78480a9b0104d915d2a4c1b6
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-05-28 18:17:37 +0200

    linting issue :white_check_mark:

--- wee/frontend/src/app/(landing)/login/page.tsx
+++ wee/frontend/src/app/(landing)/login/page.tsx
@@ -5,34 +5,31 @@ import Link from 'next/link'
 
 export default function Login() {
     return (
+      <div className="min-h-[calc(100vh-13rem)] w-full flex flex-col justify-between sm:min-h-[calc(100vh-18rem)] md:min-h-full">
+        <div className="">
+          <ThemeSwitch />
+          <h1 className="text-center mt-4 font-poppins-bold text-2xl text-jungleGreen-800 dark:text-dark-primaryTextColor">
+            Ready to Dive Back In?
+          </h1>
+          <h3 className="text-center font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">
+            Log in to your account and let&apos;s get started!
+          </h3>
 
-            <div className="min-h-[calc(100vh-13rem)] w-full flex flex-col justify-between sm:min-h-[calc(100vh-18rem)] md:min-h-full">
-                <div className="">
-                    <ThemeSwitch/>
-                    <h1 className="text-center mt-4 font-poppins-bold text-2xl text-jungleGreen-800 dark:text-dark-primaryTextColor">Ready to Dive Back In?</h1>
-                    <h3 className="text-center font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">Log in to your account and let's get started!</h3>
+          <p>Input</p>
+          <p>Input</p>
+          <p>Button</p>
+        </div>
 
-                    <p>Input</p>
-                    <p>Input</p>
-                    <p>Button</p>
-                </div>
-                
-
-                <div className="text-center font-poppins-regular text-jungleGreen-800 dark:text-dark-primaryTextColor">
-                    <span>
-                        Don't have an account?
-                    </span> 
-                    <span className="font-poppins-medium underline underline-offset-4 decoration-2 ml-2 hover:cursor-pointer dark:text-jungleGreen-150">
-                        <Link href={"/signup"}>
-                            Sign up
-                        </Link>
-                    </span>
-                </div>
-
-            </div>
-        
-    )
+        <div className="text-center font-poppins-regular text-jungleGreen-800 dark:text-dark-primaryTextColor">
+          <span>Don&apos;t have an account?</span>
+          <span className="font-poppins-medium underline underline-offset-4 decoration-2 ml-2 hover:cursor-pointer dark:text-jungleGreen-150">
+            <Link href={'/signup'}>Sign up</Link>
+          </span>
+        </div>
+      </div>
+    );
 }
+
 {/* <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 533.5 544.3"
