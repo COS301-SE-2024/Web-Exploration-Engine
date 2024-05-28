b4480994faec30191948f88bbc70463d0a964337
katnicole14 <u22543946@tuks.co.za>
2024-05-28 15:46:53 +0200

    better version that return paths that can be scrapped

--- wee/webscraper/src/robots-app/robots.ts
+++ wee/webscraper/src/robots-app/robots.ts
@@ -1,32 +1,32 @@
 import fetch from 'node-fetch';
 
 export async function extractAllowedPaths(url: string): Promise<Set<string>> {
-
   const domain = extractDomain(url);
   const robotstxtUrl = `${domain}/robots.txt`;
 
   const response = await fetch(robotstxtUrl);
   const robotstxt = await response.text();
 
+  let isGlobalUserAgent = false;
+
   const lines = robotstxt.split('\n');
   const allowedPaths = new Set<string>();
 
   lines.forEach((line) => {
-    if (line.startsWith('Allow:')) {
+    line = line.trim();
+    if (line.startsWith('User-agent:')) {
+      const userAgent = line.substring(11).trim();
+      isGlobalUserAgent = userAgent === '*';
+    } else if (line.startsWith('Allow:') && isGlobalUserAgent) {
       let path = line.substring(6).trim();
       if (path.includes('*')) {
-        // Remove everything after the asterisk (*) also handle this differently in the future
+        // Remove everything after the asterisk (*) - handle this differently in the future
         path = path.substring(0, path.indexOf('*'));
-    }
+      }
       if (path.startsWith('/')) {
         allowedPaths.add(path);
       }
     }
-  });
-
-  console.log('Extracted Paths:');
-  allowedPaths.forEach((path) => {
-    console.log(path);
   });
 
   return allowedPaths;
@@ -34,10 +34,10 @@ export async function extractAllowedPaths(url: string): Promise<Set<string>> {
 
 function extractDomain(url: string): string {
   try {
-      const parsedUrl = new URL(url);
-      return parsedUrl.origin;
+    const parsedUrl = new URL(url);
+    return parsedUrl.origin;
   } catch (error) {
-      console.error('Invalid URL:', error);
-      throw new Error('Invalid URL');
+    console.error('Invalid URL:', error);
+    throw new Error('Invalid URL');
   }
 }
