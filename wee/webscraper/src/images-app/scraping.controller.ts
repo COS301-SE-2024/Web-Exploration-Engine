22918f0d296c41e22cb9feee76f1f0334f7d87da
Johane-B <JohAnkia@2>
2024-05-28 19:32:36 +0200

    scrape images and logos

---
+++ wee/webscraper/src/images-app/scraping.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { isCrawlingAllowed } from './robot';
+import { ScrapingService } from './scraping.service';
+
+@Controller()
+export class ScrapingController {
+    constructor(private readonly scrapingService: ScrapingService) {}
+
+    @Get('/isCrawlingAllowed')
+    async isCrawlingAllowed(@Query('url') url: string): Promise<boolean> {
+        return isCrawlingAllowed(url);
+    }
+
+    @Get('/scrapeImages')
+    async scrapeImages(@Query('url') url: string): Promise<string[]> {
+        return this.scrapingService.scrapeImages(url);
+    }
+
+    @Get('/scrapeLogos')
+    async scrapeLogos(@Query('url') url: string): Promise<string> { 
+        return this.scrapingService.scrapeLogos(url);
+    }
+}
