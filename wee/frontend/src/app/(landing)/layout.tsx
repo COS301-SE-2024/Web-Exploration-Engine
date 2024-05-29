928b315edf57ffcf6ef07a81ef9c71d35a477cb4
MignonErasmus <u22492586@tuks.co.za>
2024-05-29 07:43:05 +0200

    improved mobile layout and made lefthand text bigger for large screens...

--- wee/frontend/src/app/(landing)/layout.tsx
+++ wee/frontend/src/app/(landing)/layout.tsx
@@ -15,13 +15,13 @@ export default function RootLayout({
             <BackgroundGradientAnimation>
               <div className="absolute z-50 inset-0 md:flex md:min-h-screen">
 
-                <div className='text-white min-h-[10rem] p-4 font-poppins-bold text-2xl sm:min-h-[15rem] md:flex-1 md:text-4xl md:m-auto'>
+                <div className='text-white min-h-[8rem] p-4 font-poppins-bold text-2xl sm:min-h-[15rem] md:flex-1 md:text-4xl lg:text-5xl md:m-auto'>
                   <p>Efficiently</p>
                   <FlipWords words={['categorize', 'analyze', 'extract']}/> 
                   <p>data from the web</p>              
                 </div>
 
-                <div className='p-4 bg-primaryBackgroundColor text-primaryTextColor rounded-t-2xl min-h-[calc(100vh-10rem)] dark:bg-dark-primaryBackgroundColor dark:text-dark-primaryTextColor sm:min-h-[calc(100vh-15rem)] md:flex-1 md:rounded-l-2xl md:rounded-r-none'>
+                <div className='p-4 bg-primaryBackgroundColor text-primaryTextColor rounded-t-2xl min-h-[calc(100vh-8rem)] dark:bg-dark-primaryBackgroundColor dark:text-dark-primaryTextColor sm:min-h-[calc(100vh-15rem)] md:flex-1 md:rounded-l-2xl md:rounded-r-none'>
                   {children}
                 </div>
                 
