3f7fdd2cd403e1918d453dfe2b0aa5cfcfb4ae98
CaitMS <caitymsimon@gmail.com>
2024-05-30 20:25:33 +0200

    restored lost imports

--- wee/webscraper/src/main.ts
+++ wee/webscraper/src/main.ts
@@ -6,7 +6,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './images-app/app.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
