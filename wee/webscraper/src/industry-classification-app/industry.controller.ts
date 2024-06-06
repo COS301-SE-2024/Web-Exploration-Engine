eda9cc07aa425e978875f97cd675fa0f22e39e16
katnicole14 <u22543946@tuks.co.za>
2024-06-06 19:27:54 +0200

    working industry classification using hugging face

--- wee/webscraper/src/industry-classification-app/industry.controller.ts
+++ wee/webscraper/src/industry-classification-app/industry.controller.ts
@@ -3,36 +3,53 @@ import { Response } from 'express';
 import { ApiTags, ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
 import { IndustryService } from '../industry-classification-app/industry.service';
 
+
 @ApiTags('Industry')
 @Controller('scrapeIndustry')
 export class IndustryController {
   constructor(private readonly scrapingService: IndustryService) {}
 
   @ApiOperation({ summary: 'Scrape metadata from a given URL' })
-  @ApiQuery({ name: 'url', required: true, description: 'The URL to scrape metadata from' })
+  @ApiQuery({
+    name: 'url',
+    required: true,
+    description: 'The URL to scrape metadata from',
+  })
   @ApiResponse({ status: 200, description: 'Metadata successfully scraped' })
   @ApiResponse({ status: 400, description: 'Bad Request. URL is required' })
-  @ApiResponse({ status: 500, description: 'Internal Server Error. Cannot scrape this website' })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error. Cannot scrape this URL',
+  })
   @Get()
   async handleScrapeMetadata(@Query('url') url: string, @Res() res: Response) {
     if (!url) {
-      return res.status(HttpStatus.BAD_REQUEST).json({ error: 'URL is required' });
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ error: 'URL is required' });
     }
 
     try {
       const metadata = await this.scrapingService.scrapeMetadata(url);
       res.status(HttpStatus.OK).json(metadata);
     } catch (error) {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: 'Cannot Scrape this Website' });
+      console.error('Error scraping metadata:', error);
+      res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ error: 'Cannot Scrape this URL' });
     }
   }
 
   @ApiOperation({ summary: 'Check if scraping is allowed for a given URL' })
-  @ApiQuery({ name: 'url', required: true, description: 'The URL to check for scraping permission' })
+  @ApiQuery({
+    name: 'url',
+    required: true,
+    description: 'The URL to check for scraping permission',
+  })
   @ApiResponse({ status: 200, description: 'Scraping permission status' })
   @Get('check-allowed')
   async checkAllowed(@Query('url') url: string): Promise<{ allowed: boolean }> {
-    const allowed = await this.scrapingService.checkAllowed(url);
+    const allowed = await IndustryService.checkAllowed(url);
     return { allowed };
   }
 }
--- wee/webscraper/src/main.ts
+++ wee/webscraper/src/main.ts
@@ -8,7 +8,9 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 
+dotenv.config({ path: __dirname + '/../.env'});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
