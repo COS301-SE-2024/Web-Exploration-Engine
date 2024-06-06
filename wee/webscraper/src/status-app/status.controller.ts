127b40e8adf0229adf864f848a9ae63a3059fce5
Johane-B <u22488287@tuks.co.za>
2024-06-06 07:08:51 +0200

    updated to scrape even if one of the urls don't allow it.

--- wee/webscraper/src/status-app/status.controller.ts
+++ wee/webscraper/src/status-app/status.controller.ts
@@ -1,33 +1,23 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
-import { isCrawlingAllowed } from './robot';
 import { StatusService } from './status.service';
 
 /**
- * Controller responsible for handling scraping-related HTTP requests.
+ * Controller responsible for handling status-related HTTP requests.
  */
 @ApiTags('Status')
 @Controller()
 export class StatusController {
-    constructor(private readonly scrapingService: StatusService) {}
-
-    @ApiOperation({ summary: 'Check if crawling is allowed for given URLs' })
-    @ApiQuery({ name: 'urls', description: 'The URLs to check crawling permissions for', required: true, type: [String] })
-    @ApiResponse({ status: 200, description: 'Returns an array of booleans indicating if crawling is allowed for each URL.', type: [Boolean] })
-    @ApiResponse({ status: 403, description: 'Crawling not allowed or robots.txt not accessible.' })
-    @Get('/isCrawlingAllowed')
-    async isCrawlingAllowed(@Query('urls') urls: string[]): Promise<boolean[]> {
-        const crawlingPromises = urls.map(url => isCrawlingAllowed(url));
-        return Promise.all(crawlingPromises);
-    }
+    constructor(private readonly statusService: StatusService) {}
 
     @ApiOperation({ summary: 'Check the status of websites at given URLs' })
-    @ApiQuery({ name: 'urls', description: 'The URLs of the websites to check', required: true, type: [String] })
+    @ApiQuery({ name: 'urls', description: 'Comma-separated list of URLs of the websites to check', required: true, type: String })
     @ApiResponse({ status: 200, description: 'Returns an array of booleans indicating if each website is reachable and status is 2xx.', type: [Boolean] })
     @ApiResponse({ status: 500, description: 'Failed to check the website status.' })
     @Get('/status')
-    async checkWebsiteStatus(@Query('urls') urls: string[]): Promise<boolean[]> {
-        const statusPromises = urls.map(url => this.scrapingService.status(url));
+    async checkWebsiteStatus(@Query('urls') urls: string): Promise<boolean[]> {
+        const urlsArray = urls.split(',');
+        const statusPromises = urlsArray.map(url => this.statusService.status(url));
         return Promise.all(statusPromises);
     }
 }
