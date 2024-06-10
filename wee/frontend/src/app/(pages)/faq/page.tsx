e669a900792a2b828948405566dc900e57004853
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-10 16:00:22 +0200

    header :white_check_mark:

--- wee/frontend/src/app/(pages)/faq/page.tsx
+++ wee/frontend/src/app/(pages)/faq/page.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 export default function Faq() {
     return (
-        <div>
-            FAQ Page
+      <div className="">
+        <div className="my-16 text-center">
+          <h1 className="my-4 font-poppins-bold text-5xl md:text-6xl text-jungleGreen-800 dark:text-dark-primaryTextColor">
+            Frequently Asked Questions
+          </h1>
+          <h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">
+            Start by entering the URL of the website you wish to scrape
+          </h3>
         </div>
-    )
+      </div>
+    );
 }
