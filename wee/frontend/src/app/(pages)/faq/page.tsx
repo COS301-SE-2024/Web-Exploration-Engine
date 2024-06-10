adf864baec52be200794ecc1ab483452ef56567c
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-10 18:31:12 +0200

    add dynamic FAQs

--- wee/frontend/src/app/(pages)/faq/page.tsx
+++ wee/frontend/src/app/(pages)/faq/page.tsx
@@ -4,10 +4,21 @@ import { Accordion, AccordionItem } from '@nextui-org/accordion';
   const defaultContent =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
+    const faqs = [
+  {
+    question: 'What is web scraping?',
+    answer: 'Web scraping is the process of extracting data from websites.',
+  },
+  {
+    question: 'Is web scraping legal?',
+    answer:
+      "Web scraping is legal as long as it is done within the boundaries of the law and respects the website's terms of service.",
+  },
+  // Add more FAQs here
+];
 export default function Faq() {
     return (
       <div className="">
-        
         <div className="my-16 text-center">
           <h1 className="my-4  mx-9 font-poppins-bold text-5xl md:text-6xl text-jungleGreen-800 dark:text-dark-primaryTextColor">
             Frequently Asked Questions
@@ -17,6 +28,18 @@ export default function Faq() {
           </h3>
         </div>
 
+        <Accordion>
+          {faqs.map((faq, index) => (
+            <AccordionItem
+              key={index}
+              aria-label={`Accordion ${index + 1}`}
+              title={faq.question}
+            >
+              {faq.answer}
+            </AccordionItem>
+          ))}
+        </Accordion>
+        
         <Accordion>
           <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
             {defaultContent}
