9fa83230823505c989977abece606de152e42704
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-10 19:58:03 +0200

    add new accordion section

--- wee/frontend/src/app/(pages)/faq/page.tsx
+++ wee/frontend/src/app/(pages)/faq/page.tsx
@@ -52,8 +52,34 @@ const faqs = [
 ];
 
 export default function Faq() {
-    return (
-      <div className="">
+  return (
+    <div className="">
+      <div className="my-16 text-center">
+        <h1 className="my-4  mx-9 font-poppins-bold text-5xl md:text-6xl text-jungleGreen-800 dark:text-dark-primaryTextColor">
+          Frequently Asked Questions
+        </h1>
+        <h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">
+          How can we help you?{' '}
+        </h3>
+      </div>
+
+      <div className="mx-auto px-10 md:px-32 lg:px-48 align-middle ">
+        <Accordion className="mx-auto ">
+          {faqs.map((faq, index) => (
+            <AccordionItem
+              className=""
+              key={index}
+              aria-label={`Accordion ${index + 1}`}
+              title={faq.question}
+            >
+              {faq.answer}
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+
+      <div id="report"></div>
+      <div id="sect-report" className="">
         <div className="my-16 text-center">
           <h1 className="my-4  mx-9 font-poppins-bold text-5xl md:text-6xl text-jungleGreen-800 dark:text-dark-primaryTextColor">
             Frequently Asked Questions
