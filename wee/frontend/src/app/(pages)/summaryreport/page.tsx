cffd40c3de68c1ea9e63fa96979f3d1264f72141
MignonErasmus <u22492586@tuks.co.za>
2024-06-11 14:09:05 +0200

    added piechart component, and chart interface/model, fixed robot.txt test

--- wee/frontend/src/app/(pages)/summaryreport/page.tsx
+++ wee/frontend/src/app/(pages)/summaryreport/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ExampleChart } from '../../components/chart';
+import { PieChart } from '../../components/Graphs';
 export default function SummaryReport() {
     return (
         <div>
             <ExampleChart/>
-            SummaryReport page
+            <PieChart dataLabel={['orange', 'lemon', 'apple']} dataSeries={[23,54,23]}/>
         </div>
     )
 }
---
+++ wee/frontend/src/app/components/Graphs/PieChart.tsx
@@ -0,0 +1,55 @@
+'use client'
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { ApexOptions } from "apexcharts";
+import IChart from "../../models/ChartModel";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+export function PieChart({dataLabel, dataSeries}: IChart) {
+    const { theme } = useTheme();
+    const [options, setOptions] = useState<ApexOptions>({
+        chart: {
+            id: 'apexchart-pie',
+            fontFamily: "'Poppins', sans-serif",
+            background: 'transparent',
+            toolbar: {
+              tools: {
+                zoom:false,
+                zoomin:true,
+                zoomout:true,
+                pan:false,
+                reset:false,
+                download:false,
+              }
+            }
+        },
+        labels: dataLabel,
+        theme: {
+            mode: theme === 'dark' ? 'dark' : 'light'
+        },
+        legend: {
+          position: 'right',
+          horizontalAlign: 'center',
+        },
+  
+    });
+
+    const series = dataSeries;
+
+    useEffect(() => {
+        setOptions(prevOptions => ({
+            ...prevOptions,
+            theme: {
+                mode: theme === 'dark' ? 'dark' : 'light'
+            }
+        }));
+    }, [theme]);
+
+    return (
+        <>
+            <Chart type="pie" options={options} series={series} height={300} width={400}/>
+        </>
+    );
+}
