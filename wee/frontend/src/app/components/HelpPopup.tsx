ba139e3dee2442dc8a6aab895b47ff8e37aeea52
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-11 12:59:44 +0200

    delete modal footers and headings

--- wee/frontend/src/app/components/HelpPopup.tsx
+++ wee/frontend/src/app/components/HelpPopup.tsx
@@ -31,29 +31,18 @@ export default function App() {
         <ModalContent className="">
           {(onClose) => (
             <>
-          {/*     <ModalHeader className="flex flex-col gap-1">
-                Modal Title
-              </ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
+        
+                <h1 className="my-4 text-center font-poppins-bold text-2xl text-jungleGreen-800 dark:text-dark-primaryTextColor">
+                  Help
+                </h1>
+                
+              <ModalBody className="px-3">
+                <h3 className="py-3 font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">
+                  Start by entering the URL of the website you wish to scrape
+                </h3>
+                
               </ModalBody>
-            {/*   <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter> */}
+         
             </>
           )}
         </ModalContent>
