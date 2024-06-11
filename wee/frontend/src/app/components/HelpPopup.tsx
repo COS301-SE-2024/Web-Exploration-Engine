05ef8a24dc411d032a7ca02ede22e736e27620aa
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-11 14:08:43 +0200

    change header text of modal

--- wee/frontend/src/app/components/HelpPopup.tsx
+++ wee/frontend/src/app/components/HelpPopup.tsx
@@ -48,11 +48,10 @@ export default function App() {
         <ModalContent className="">
           {(onClose) => (
             <>
-        
-                <h1 className="my-4 text-center font-poppins-bold text-2xl text-jungleGreen-800 dark:text-dark-primaryTextColor">
-                  Help
-                </h1>
-                
+              <h1 className="my-4 text-center font-poppins-bold text-2xl text-jungleGreen-800 dark:text-dark-primaryTextColor">
+                Help
+              </h1>
+
               <ModalBody className="px-3">
                 <h3 className="py-3 font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">
                   Start by entering the URL of the website you wish to scrape
