81fbf706566c6f84d6848abe3248b0c729d51ab9
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-11 10:25:23 +0200

    remove all default styles

--- wee/frontend/src/app/components/HelpPopup.tsx
+++ wee/frontend/src/app/components/HelpPopup.tsx
@@ -14,32 +14,22 @@ import {
 
 export default function App() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  const [modalPlacement, setModalPlacement] = React.useState('auto');
 
   return (
     <div className="flex flex-col gap-2">
       <Button onPress={onOpen} className="max-w-fit">
         Open Modal
       </Button>
-      <RadioGroup
-        label="Select modal placement"
-        orientation="horizontal"
-        value={modalPlacement}
-        onValueChange={setModalPlacement}
-      >
-        <Radio value="auto">auto</Radio>
-        <Radio value="top">top</Radio>
-        <Radio value="bottom">bottom</Radio>
-        <Radio value="center">center</Radio>
-        <Radio value="top-center">top-center</Radio>
-        <Radio value="bottom-center">bottom-center</Radio>
-      </RadioGroup>
+    
       <Modal
         isOpen={isOpen}
-        placement={modalPlacement}
+        placement={"bottom"}
+        backdrop="transparent"
+        
         onOpenChange={onOpenChange}
+        className="border-2 border-jungleGreen-600 bg-white"
       >
-        <ModalContent>
+        <ModalContent className="">
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col gap-1">
