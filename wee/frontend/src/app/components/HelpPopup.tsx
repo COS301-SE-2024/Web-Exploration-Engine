3886a9e910ddec16cac6087e2860758d5b163112
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-12 19:54:22 +0200

    replace svg arrow

--- wee/frontend/src/app/components/HelpPopup.tsx
+++ wee/frontend/src/app/components/HelpPopup.tsx
@@ -88,19 +88,7 @@ export default function App() {
                   <h3 className=" font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">
                     Provide Feedback
                   </h3>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="jungleGreen-200"
-                    className="border border-jungleGreen-200"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
-                    />
-                  </svg>
+                  <h3 className="font-poppins-semibold text-xl">↗</h3>
                 </div>
               </ModalBody>
             </>
