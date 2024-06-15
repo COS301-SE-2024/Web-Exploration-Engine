e1b1bdda8459e69203806ca33053bc69737a121f
CaitMS <caitymsimon@gmail.com>
2024-06-15 16:02:05 +0200

    logo tests added

--- wee/webscraper/src/scraper/scrape-logo/scrape-logo.service.spec.ts
+++ wee/webscraper/src/scraper/scrape-logo/scrape-logo.service.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ScrapeLogoService } from './scrape-logo.service';
 import { Metadata, RobotsResponse } from '../models/ServiceModels';
+import * as puppeteer from 'puppeteer';
+
+jest.mock('axios');
+
+jest.mock('puppeteer');
+const mockedPuppeteer = puppeteer as jest.Mocked<typeof puppeteer>;
 
 describe('ScrapeLogoService', () => {
   let service: ScrapeLogoService;
@@ -77,5 +83,75 @@ describe('ScrapeLogoService', () => {
     expect(result).toBe('');
   });
 
-  // Add more tests for different scenarios (e.g., multiple images matching, error handling)
+  // it('should return first image URL that matches the pattern "logo"', async () => {
+
+  //   const browser = {
+  //     newPage: jest.fn().mockResolvedValue({
+  //       goto: jest.fn(),
+  //       evaluate: jest.fn()
+  //         .mockResolvedValueOnce({ ogImage: '' })
+  //         .mockResolvedValueOnce(['http://example.com/logo1.png', 'http://example.com/logo2.png']),
+  //       close: jest.fn(),
+  //     }),
+  //     close: jest.fn(),
+  //   };
+  //   mockedPuppeteer.launch.mockResolvedValue(browser as any);
+
+  //   const metadata: Metadata = {
+  //     title: 'Test Title',
+  //     description: 'Test Description',
+  //     keywords: 'Test Keywords',
+  //     ogTitle: 'Test OG Title',
+  //     ogDescription: 'Test OG Description',
+  //     ogImage: '',
+  //   };
+  //   const robots: RobotsResponse = {
+  //     baseUrl: 'http://example.com',
+  //     allowedPaths: [],
+  //     disallowedPaths: [],
+  //     isBaseUrlAllowed: true,
+  //     isUrlScrapable: true,
+  //   };
+
+  //   const result = await service.scrapeLogo('http://example.com', metadata, robots);
+  //   expect(result).toBe('http://example.com/logo1.png');
+  //   expect(browser.newPage).toHaveBeenCalledTimes(1);
+  //   expect(browser.close).toHaveBeenCalledTimes(1);
+  // });
+
+  it('should return empty string if no logo is found', async () => {
+
+    const browser = {
+      newPage: jest.fn().mockResolvedValue({
+        goto: jest.fn(),
+        evaluate: jest.fn()
+          .mockResolvedValueOnce({ ogImage: null })
+          .mockResolvedValueOnce([]),
+        close: jest.fn(),
+      }),
+      close: jest.fn(),
+    };
+    mockedPuppeteer.launch.mockResolvedValue(browser as any);
+
+    const metadata: Metadata = {
+      title: 'Test Title',
+      description: 'Test Description',
+      keywords: 'Test Keywords',
+      ogTitle: 'Test OG Title',
+      ogDescription: 'Test OG Description',
+      ogImage: '',
+    };
+    const robots: RobotsResponse = {
+      baseUrl: 'http://example.com',
+      allowedPaths: [],
+      disallowedPaths: [],
+      isBaseUrlAllowed: true,
+      isUrlScrapable: true,
+    };
+
+    const result = await service.scrapeLogo('http://example.com', metadata, robots);
+    expect(result).toEqual("");
+    expect(browser.newPage).toHaveBeenCalledTimes(1);
+    expect(browser.close).toHaveBeenCalledTimes(1);
+  });
 });
