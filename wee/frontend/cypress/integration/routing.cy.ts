46b08db2683d12777f355d448d74a3d023ba9a27
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-18 16:41:57 +0200

    extend route test

--- wee/frontend/cypress/integration/routing.cy.ts
+++ wee/frontend/cypress/integration/routing.cy.ts
@@ -1,5 +1,6 @@
 describe('frontend', () => {
-  it('should visit site', () => {
+  it('be able to visit all pages', () => {
     cy.visit('/');
+    cy.contains(/web exploration engine/i).should('exist');
   });
 });
