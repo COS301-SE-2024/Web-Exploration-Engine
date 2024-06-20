df66524fdc35d48c308a37d000397b51e945a279
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-20 07:11:29 +0200

    add cypress test files

--- wee/frontend/specs/component/Login.spec.tsx
+++ wee/frontend/specs/component/Login.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent, act } from '@testing-library/react';
 import Login from '../../src/app/(landing)/login/page';
 import { login } from '../../src/app/services/AuthService';
-import { useRouter } from 'next/navigation'; 
+import { useRouter } from 'next/navigation';
 
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn(),
@@ -13,14 +13,12 @@ jest.mock('../../src/app/services/AuthService', () => ({
   login: jest.fn(),
 }));
 
-
 describe('Login Component', () => {
-
-  it('should render the Login form', async () =>  {
+  it('should render the Login form', async () => {
     await act(async () => {
       render(<Login />);
     });
-
+ 
     expect(screen.getByLabelText(/Email/i)).toBeDefined();
     expect(screen.getByLabelText(/Password/i)).toBeDefined();
   });
@@ -30,33 +28,44 @@ describe('Login Component', () => {
 
     fireEvent.click(screen.getByText(/^Login$/));
 
-
     expect(screen.queryByText(/All fields are required/i)).toBeDefined();
   });
 
   it('should display error if email is invalid', async () => {
     render(<Login />);
 
-    fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: 'invalid-email' } });
-    fireEvent.change(screen.getByLabelText(/Password/i), { target: { value: 'password' } });
+    fireEvent.change(screen.getByLabelText(/Email/i), {
+      target: { value: 'invalid-email' },
+    });
+    fireEvent.change(screen.getByLabelText(/Password/i), {
+      target: { value: 'password' },
+    });
 
     fireEvent.click(screen.getByText(/^Login$/));
 
-    expect(screen.queryByText(/Please enter a valid email address/i)).toBeDefined();
+    expect(
+      screen.queryByText(/Please enter a valid email address/i)
+    ).toBeDefined();
   });
 
   it('should call login function on valid submission', async () => {
     const push = jest.fn();
     (useRouter as jest.Mock).mockReturnValue({ push });
 
-    (login as jest.Mock).mockResolvedValue({ accessToken: 'mockToken', uuid: 'mockUuid' });
+    (login as jest.Mock).mockResolvedValue({
+      accessToken: 'mockToken',
+      uuid: 'mockUuid',
+    });
 
-        
     await act(async () => {
       render(<Login />);
     });
-    fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: 'test@example.com' } });
-    fireEvent.change(screen.getByLabelText(/Password/i), { target: { value: 'password' } });
+    fireEvent.change(screen.getByLabelText(/Email/i), {
+      target: { value: 'test@example.com' },
+    });
+    fireEvent.change(screen.getByLabelText(/Password/i), {
+      target: { value: 'password' },
+    });
     const button = screen.getByTestId('login-button');
     expect(button).toBeDefined();
 
@@ -65,7 +74,9 @@ describe('Login Component', () => {
       email: 'test@example.com',
       password: 'password',
     });
-    expect(screen.queryByText(/An error occurred. Please try again later/i)).toBeNull();
+    expect(
+      screen.queryByText(/An error occurred. Please try again later/i)
+    ).toBeNull();
     // expect(push).toHaveBeenCalled();
   });
 });
