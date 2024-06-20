d2eaf3f257a8b54cf29b3fc204d351e96a7004c1
Johane-B <u22488287@tuks.co.za>
2024-06-20 18:26:42 +0200

    added filtering with status

--- wee/frontend/src/app/(pages)/scraperesults/page.tsx
+++ wee/frontend/src/app/(pages)/scraperesults/page.tsx
@@ -20,12 +20,25 @@ function ResultsComponent() {
 
     const [searchValue, setSearchValue] = React.useState("");
     const hasSearchFilter = Boolean(searchValue);
-    
+    const [selectedStatusFilter, setSelectedStatusFilter] = React.useState("");
+
     const router = useRouter();
 
     const filteredItems = React.useMemo(() => {
         let filteredUrls = [...results];
 
+        // Apply status filter
+        if (selectedStatusFilter === "Parked") {
+            filteredUrls = filteredUrls.filter((url) =>
+                url.domainStatus === "parked"
+            );
+        } else if (selectedStatusFilter === "Live") {
+            filteredUrls = filteredUrls.filter((url) =>
+                url.domainStatus === "live"
+            );
+        }
+
+        // Apply search filter
         if (hasSearchFilter) {
             filteredUrls = filteredUrls.filter((url) =>
                 url.url.toLowerCase().includes(searchValue.toLowerCase()),
@@ -33,7 +46,7 @@ function ResultsComponent() {
         }
     
         return filteredUrls;
-    }, [results, searchValue]);
+    }, [results, searchValue, selectedStatusFilter]);
     
 
     const handleResultPage = (url:string) => {
@@ -70,7 +83,7 @@ function ResultsComponent() {
             });
         }  
         else {
-            // allows to naviagte back to this page without rescraping the urls
+            // allows to navigate back to this page without rescraping the urls
             if (processedUrls.current.size > 1) {
                 // Generate summary report
                 console.log('Results:', results)
@@ -86,7 +99,7 @@ function ResultsComponent() {
         console.log("Results changed!!!!")  
         if (urls.length === results.length) {
             setIsLoading(false);  
-            // allows to naviagte back to this page without rescraping the urls  
+            // allows to navigate back to this page without rescraping the urls  
             setUrls([]);
         }
     }, [results])
@@ -110,6 +123,11 @@ function ResultsComponent() {
         } else {
           setSearchValue("");
         }
+    };
+
+    const handleStatusFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const status = event.target.value;
+        setSelectedStatusFilter(status);
     };
 
     const handleSummaryPage = () => {
@@ -137,6 +155,7 @@ function ResultsComponent() {
                 <WEESelect
                     label="Live/Parked"
                     className="w-full pb-3 md:w-1/3"
+                    onChange={handleStatusFilterChange}
                 >
                     <SelectItem key={"Parked"}>Parked</SelectItem>
                     <SelectItem key={"Live"}>Live</SelectItem>
@@ -177,7 +196,7 @@ function ResultsComponent() {
                 aria-label="Scrape result table"
                 bottomContent={
                     <>
-                    
+
                         {isLoading ? (
                             <div className="flex w-full justify-center">
                                 <Spinner color='default'/>
@@ -198,12 +217,12 @@ function ResultsComponent() {
                             </div>
                         }
                     </>
-                  
+
                 }
                 classNames={{
                     wrapper: "min-h-[222px]",
                 }}
-                >
+            >
                 <TableHeader>
                     <TableColumn key="name" className='rounded-lg sm:rounded-none'>
                         URL
@@ -217,7 +236,7 @@ function ResultsComponent() {
                 </TableHeader>
 
                 <TableBody
-                    emptyContent={"There is no results to be displayed"}
+                    emptyContent={"There are no results to be displayed"}
                 >
                     {items.map((item, index) => (
                         <TableRow key={index}>
@@ -227,7 +246,7 @@ function ResultsComponent() {
                                 </Link>
                             </TableCell>
                             <TableCell className='text-center hidden sm:table-cell'>
-                                <Chip radius="sm" color={item.robots.isUrlScrapable? 'success' : 'warning'} variant="flat">{item.robots.isUrlScrapable ? 'Yes' : 'No'}</Chip>
+                                <Chip radius="sm" color={item.domainStatus === "live" ? 'success' : 'warning'} variant="flat">{item.domainStatus === "live" ? 'Yes' : 'No'}</Chip>
                             </TableCell>
                             <TableCell className='text-center hidden sm:table-cell'>
                                 <Button className="font-poppins-semibold bg-jungleGreen-700 text-dark-primaryTextColor dark:bg-jungleGreen-400 dark:text-primaryTextColor"
