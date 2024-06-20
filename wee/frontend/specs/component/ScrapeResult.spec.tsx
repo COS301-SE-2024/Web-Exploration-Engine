1de4f8046794f75d0ac4e281c09f6d13347a258b
CaitMS <caitymsimon@gmail.com>
2024-06-20 21:31:53 +0200

    fixed failing test

--- wee/frontend/specs/component/ScrapeResult.spec.tsx
+++ wee/frontend/specs/component/ScrapeResult.spec.tsx
@@ -86,12 +86,4 @@ describe('Scrape Results Component', () => {
     
         jest.spyOn(React, 'useMemo').mockImplementation(() => mockFilteredItems);
     });
-
-    it('should not make API calls if the URL is already processed', async () => {
-        
-        await act(async () => {
-            render(<ScrapeResults />);
-        });
-        
-    });
 });
