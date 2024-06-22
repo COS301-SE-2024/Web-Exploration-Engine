2607ca03005e70b3ef0095eaf0fbfafc5e5b7508
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-22 16:56:47 +0200

    Update help.cy.ts

--- wee/frontend/cypress/integration/help.cy.ts
+++ wee/frontend/cypress/integration/help.cy.ts
@@ -4,4 +4,11 @@ describe('help page', () => {
     cy.contains(/Frequently asked questions/i).should('exist');
     cy.contains(/Feedback/i).should('exist');
   });
+
+  it('help should be available', () => {
+    cy.visit('/help');
+    cy.get('[data-testid="help-button"]').should('exist');
+  });
+
+  
 });
