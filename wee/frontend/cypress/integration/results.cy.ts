28176eb9036534445f679caa5f64f59ba423f703
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-22 16:56:50 +0200

    Update results.cy.ts

--- wee/frontend/cypress/integration/results.cy.ts
+++ wee/frontend/cypress/integration/results.cy.ts
@@ -1,7 +1,36 @@
 describe('results', () => {
+  /*   beforeEach(() => {
+    cy.visit('results?url=');
+  }); */
+
   it('should be on right page', () => {
-    cy.visit('/results?url=');
+    cy.visit('/results?url='); //results?url= should be in before each
     cy.contains(/General overview/i).should('exist');
     cy.contains(/No images available/i).should('exist');
+  });
+
+  it('should get correct github.com response', () => {
+    cy.visit('/results?url=https%3A%2F%2Fgithub.com');
+  });
+
+  it('should get correct unsplash.com response', () => {
+    // http://localhost:3001/results?url=https%3A%2F%2Funsplash.com%2F
+    cy.visit('/results?url=https%3A%2F%2Funsplash.com%2F');
+  });
+
+  it('should get correct chatgbt.com response - unscrapable', () => {
+    cy.visit('/');
+  });
+
+  it('should get correct unsplash.com response', () => {
+    cy.fixture('individual-scrapes/unsplash')
+      .as('jsonUnsplah')
+      .then((jsonUnsplah) => {
+        console.log(jsonUnsplah);
+
+        cy.request(
+          'http://localhost:3001/results?url=https%3A%2F%2Fgithub.com'
+        ).as('get-github');
+      });
   });
 });
