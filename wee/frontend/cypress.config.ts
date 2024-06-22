6d40cc8df05c525d67868cf5a7241aca807893ee
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-22 17:33:50 +0200

    Update cypress.config.ts

--- wee/frontend/cypress.config.ts
+++ wee/frontend/cypress.config.ts
@@ -4,7 +4,35 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
+    },
     ...nxE2EPreset(__filename, { cypressDir: 'cypress' }),
     baseUrl: 'http://localhost:3001',
   },
-});
+});/* 
+
+// cypress.config.ts
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config
+    },
+  },
+})
+  
+*/
