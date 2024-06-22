8b50806ac1d124f680f9dbf707dd6cf242806980
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-22 16:56:21 +0200

    covered tests for demo 2 components

--- wee/frontend/cypress/integration/scraping.cy.ts
+++ wee/frontend/cypress/integration/scraping.cy.ts
@@ -1,5 +1,4 @@
-import "commands"
-describe('landing page', () => {
+describe('scraping functionality', () => {
   beforeEach(() => {
     //Get all mocks then intercept all api requests using those mocks
     cy.fixture('/api/scraper/github')
@@ -81,14 +80,16 @@ describe('landing page', () => {
     cy.contains(/The Web Exploration Engine/i).should('exist');
   });
 
-  it('successfully scrapes 1 crawlable url', () => {
+  it('help should be available', () => {
+    cy.visit('/');
+    cy.get('[data-testid="help-button"]').should('exist');
+  });
+
+  it('1 crawlable url, view results', () => {
     cy.visit('/');
 
-    //enter the url into the the box
     cy.get('[data-testid="scraping-textarea-home"]').type('https://github.com');
 
-    //then the response from there should be intercepted and return mock predictable data
-    //then
     cy.get('[data-testid="btn-start-scraping"]').click();
 
     cy.fixture('/api/scraper/github')
@@ -101,39 +102,26 @@ describe('landing page', () => {
         ).as('mock_scraper_github');
       });
 
-    cy.fixture('/api/scraper/unsplash')
-      .as('mock_scraper_unsplash')
-      .then((mock_scraper_unsplash) => {
-        cy.intercept(
-          'GET',
-          'http://localhost:3000/api/scraper?url=https%3A%2F%2Funsplash.com',
-          mock_scraper_unsplash
-        ).as('mock_scraper_unsplash');
-      });
-
-    //Then i should be on the scrape results page
-    // Wait for the intercepted request
     cy.wait('@mock_scraper_github');
 
     // Assert that the URL is the scraperesults URL
     cy.url().should('include', 'scraperesults');
 
-    //Now I'm here , I should also be able to see a table and when i click on the view button it should change the url
+    cy.get('[data-testid="btnView0"]').should('exist');
+    cy.get('[data-testid="btnView0"]').click();
 
-    cy.get('[data-testid="btnView"]').click();
-
-    //now the url should be changed
+    //Story : View results
     cy.url().should('include', 'https%3A%2F%2Fgithub.com%2F');
   });
 
-  it('successfully scrape 2 crawlable urls', () => {
+  it('2 crawlable urls, filterable results, individual reports, ', () => {
     cy.visit('/');
 
-    //enter the url into the the box
-    cy.get('[data-testid="scraping-textarea-home"]').type('https://github.com,unsplash.com');
+    //Story : Enter multiple URLs
+    cy.get('[data-testid="scraping-textarea-home"]').type(
+      'https://github.com,https://unsplash.com'
+    );
 
-    //then the response from there should be intercepted and return mock predictable data
-    //then
     cy.get('[data-testid="btn-start-scraping"]').click();
 
     cy.fixture('/api/scraper/github')
@@ -163,15 +151,27 @@ describe('landing page', () => {
     // Assert that the URL is the scraperesults URL
     cy.url().should('include', 'scraperesults');
 
-    //Now I'm here , I should also be able to see a table and when i click on the view button it should change the url
+    //Story : Filter specific URLs
+    cy.get('[data-testid="search-urls"]').type('unsplash');
 
-    cy.get('[data-testid="btnView"]').click();
+    //ensure that the button for unsplash isnt there
+    cy.contains(/git/i).should('not.exist');
+
+    //Story : View results
+    cy.get('[data-testid="btnView0"]').click();
 
     //now the url should be changed
-    cy.url().should('include', 'https%3A%2F%2Funsplash.com%2F');
+    cy.url().should('include', 'unsplash.com');
+
+    //Story : See industry classification, domain watch
+    cy.url().should('include', 'unsplash');
+    cy.contains(/results of http/i).should('exist');
+    cy.contains(/industry/i).should('exist');
+    cy.contains(/domain match/i).should('exist');
+    cy.contains(/confidence score/i).should('exist');
   });
 
-  it('successfully scrape 1 crawlable url, 1 uncrawlable url', () => {
+  it('1/2 crawlable url, view overall report', () => {
     cy.visit('/');
 
     //enter the url into the the box
@@ -206,15 +206,19 @@ describe('landing page', () => {
     //Then i should be on the scrape results page
     // Wait for the intercepted request
     cy.wait('@mock_scraper_fakewebsite');
+    cy.wait('@mock_scraper_github');
 
-    // Assert that the URL is the scraperesults URL
+    // click on report summary button
+    // Story : See overall report
     cy.url().should('include', 'scraperesults');
+    cy.get('[data-testid="btn-report-summary"]').click();
 
-    //Now I'm here , I should also be able to see a table and when i click on the view button it should change the url
-
-    cy.get('[data-testid="btnView"]').click();
-
-    //now the url should be changed
-    cy.url().should('include', 'https%3A%2F%2Ffakewebsite.co.za%2F');
+    //Story : Summary of industry classifcations, status, domain
+    cy.contains(/1/i).should('exist');
+    cy.contains(/2/i).should('exist');
+    cy.contains(/scrape time/i).should('exist');
+    cy.contains(/50/i).should('exist');
+    cy.contains(/no weak classifications/i).should('exist');
+    cy.contains(/Parked sites/i).should('exist');
   });
 }); // end of describe
