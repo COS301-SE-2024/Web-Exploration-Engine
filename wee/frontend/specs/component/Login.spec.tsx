979e0c7d3e5bb9f62063339844fb8278ed3d2db2
katnicole14 <u22543946@tuks.co.za>
2024-06-22 22:37:25 +0200

    more front-end unit tests

--- wee/frontend/specs/component/Login.spec.tsx
+++ wee/frontend/specs/component/Login.spec.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { render, screen, fireEvent, act ,waitFor} from '@testing-library/react';
 import Login from '../../src/app/(landing)/login/page';
 import { login } from '../../src/app/services/AuthService';
-import { useRouter } from 'next/navigation'; 
+import { useRouter } from 'next/navigation';
 
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn(),
@@ -24,6 +24,11 @@ describe('Login Component', () => {
     expect(screen.getByLabelText(/Email/i)).toBeDefined();
     expect(screen.getByLabelText(/Password/i)).toBeDefined();
   });
+
+
+
+
+
 
   it('should display error if required fields are empty', async () => {
     render(<Login />);
@@ -51,7 +56,7 @@ describe('Login Component', () => {
 
     (login as jest.Mock).mockResolvedValue({ accessToken: 'mockToken', uuid: 'mockUuid' });
 
-        
+
     await act(async () => {
       render(<Login />);
     });
@@ -68,4 +73,20 @@ describe('Login Component', () => {
     expect(screen.queryByText(/An error occurred. Please try again later/i)).toBeNull();
     // expect(push).toHaveBeenCalled();
   });
+  it('should redirect to home page after successful login', async () => {
+    const push = jest.fn();
+    (useRouter as jest.Mock).mockReturnValue({ push });
+    (login as jest.Mock).mockResolvedValue({ accessToken: 'mockToken', uuid: 'mockUuid' });
+
+    render(<Login />);
+
+    fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: 'test@example.com' } });
+    fireEvent.change(screen.getByLabelText(/Password/i), { target: { value: 'password' } });
+
+    fireEvent.click(screen.getByTestId('login-button'));
+
+    await waitFor(() => expect(push).toHaveBeenCalledWith('/?url=mockToken&uuid=mockUuid'));
+  });
+
+
 });
