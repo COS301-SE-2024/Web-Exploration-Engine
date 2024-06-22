95fb3518efb5a6ee93e4c0c7171f5f76f58623df
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-22 16:56:38 +0200

    Update scraperesults.cy.ts

--- wee/frontend/cypress/integration/scraperesults.cy.ts
+++ wee/frontend/cypress/integration/scraperesults.cy.ts
@@ -1,6 +1,19 @@
+//import github from '../fixtures/api-scrape/github.json';
+
 describe('scraperesults', () => {
   it('should be on right page', () => {
     cy.visit('/scraperesults');
     cy.contains(/Summary/i).should('exist');
+
+    cy.fixture('api-scrape/github')
+      .as('jsonGithub')
+      .then((jsonGithub) => {
+        console.log(jsonGithub);
+        cy.intercept(
+          'GET',
+          'http://localhost:3000/api/scraper?url=https%3A%2F%2Fgithub.com',
+          jsonGithub
+        ).as("mock");
+      }); 
   });
 });
