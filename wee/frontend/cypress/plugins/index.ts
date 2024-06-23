de95ab2d7038245684a12511eed98c4fe02e89b9
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-06-23 15:43:29 +0200

    Update index.ts

--- wee/frontend/cypress/plugins/index.ts
+++ wee/frontend/cypress/plugins/index.ts
@@ -15,6 +15,7 @@ export default function (on: any, config: any) {
   task(on, config);
   return config; // <-- REQUIRED
 }
+
 /* module.exports = (on, config) => {
   on('task', require('@cypress/code-coverage/task'));
 }; */
