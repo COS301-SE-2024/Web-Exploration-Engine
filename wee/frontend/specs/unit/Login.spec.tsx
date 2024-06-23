6a6a2f6947e5ed8cd3ca338fd551edc78517b20f
katnicole14 <u22543946@tuks.co.za>
2024-06-23 08:09:55 +0200

    frontend unit tests

--- wee/frontend/specs/unit/Login.spec.tsx
+++ wee/frontend/specs/unit/Login.spec.tsx
@@ -87,6 +87,51 @@ describe('Login Component', () => {
 
     await waitFor(() => expect(push).toHaveBeenCalledWith('/?url=mockToken&uuid=mockUuid'));
   });
+  it('should display error if invalid login credentials are provided', async () => {
+    (login as jest.Mock).mockResolvedValue({ message: 'Invalid login credentials' });
 
+    await act(async () => {
+      render(<Login />);
+    });
+    fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: 'test@example.com' } });
+    fireEvent.change(screen.getByLabelText(/Password/i), { target: { value: 'password' } });
+
+    fireEvent.click(screen.getByTestId('login-button'));
+
+    await waitFor(() => expect(screen.queryByText(/Invalid email or password/i)).toBeDefined());
+  });
+
+  it('should display error if a general error occurs', async () => {
+    (login as jest.Mock).mockResolvedValue({ code: 500 });
+
+    await act(async () => {
+      render(<Login />);
+    });
+    fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: 'test@example.com' } });
+    fireEvent.change(screen.getByLabelText(/Password/i), { target: { value: 'password' } });
+
+    fireEvent.click(screen.getByTestId('login-button'));
+
+    await waitFor(() => expect(screen.queryByText(/An error occurred. Please try again later/i)).toBeDefined());
+  });
+
+  it('should render the ThemeSwitch component', async () => {
+    await act(async () => {
+      render(<Login />);
+    });
+
+    expect(screen.getByText(/Ready to Dive Back In?/i)).toBeDefined();
+  });
+
+  it('should render third-party login buttons', async () => {
+    await act(async () => {
+      render(<Login />);
+    });
+
+    expect(screen.getByText(/Login with Google/i)).toBeDefined();
+    expect(screen.getByText(/Login with Apple/i)).toBeDefined();
+  });
+
+ 
 
 });
