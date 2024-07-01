0b973e1210e0b3680730cc097fd2960234320a76
CaitMS <caitymsimon@gmail.com>
2024-07-01 16:24:15 +0200

    Added pagination to saved report page

--- wee/frontend/src/app/(pages)/savedreports/page.tsx
+++ wee/frontend/src/app/(pages)/savedreports/page.tsx
@@ -1,9 +1,5 @@
 'use client';
-import React, { useEffect, Suspense, useRef } from 'react';
-import { FiSearch } from 'react-icons/fi';
-import { SelectItem } from '@nextui-org/react';
-import WEEInput from '../../components/Util/Input';
-import WEESelect from '../../components/Util/Select';
+import React, { Suspense } from 'react';
 import WEEPagination from '../../components/Util/Pagination';
 import {
   TableHeader,
@@ -11,164 +7,16 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  Chip,
   Button,
-  Spinner,
 } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 import WEETable from '../../components/Util/Table';
-import { useScrapingContext } from '../../context/ScrapingContext';
-import Scraping from '../../models/ScrapingModel';
 import Link from 'next/link';
-import { generateSummary } from '../../services/SummaryService';
-import { is } from 'cypress/types/bluebird';
 
 function ResultsComponent() {
-  const {
-    urls,
-    setUrls,
-    results,
-    setResults,
-    setSummaryReport,
-    processedUrls,
-    processingUrls,
-  } = useScrapingContext();
+ 
 
-  const [searchValue, setSearchValue] = React.useState('');
-  const hasSearchFilter = Boolean(searchValue);
-  const [selectedStatusFilter, setSelectedStatusFilter] = React.useState('');
-  const [selectedCrawlableFilter, setSelectedCrawlableFilter] =
-    React.useState('');
   const router = useRouter();
-
-  const filteredItems = React.useMemo(() => {
-    let filteredUrls = [...results];
-
-    // Apply status filter
-    if (selectedStatusFilter === 'Parked') {
-      filteredUrls = filteredUrls.filter(
-        (url) => url.domainStatus === 'parked'
-      );
-    } else if (selectedStatusFilter === 'Live') {
-      filteredUrls = filteredUrls.filter((url) => url.domainStatus === 'live');
-    }
-
-    // Apply crawlable filter
-    if (selectedCrawlableFilter === 'Yes') {
-      filteredUrls = filteredUrls.filter(
-        (url) => url.robots && url.robots.isUrlScrapable
-      );
-    } else if (selectedCrawlableFilter === 'No') {
-      filteredUrls = filteredUrls.filter(
-        (url) => url.robots && !url.robots.isUrlScrapable
-      );
-    }
-
-    // Apply search filter
-    if (hasSearchFilter) {
-      filteredUrls = filteredUrls.filter((url) =>
-        url.url.toLowerCase().includes(searchValue.toLowerCase())
-      );
-    }
-
-    return filteredUrls;
-  }, [results, searchValue, selectedStatusFilter, selectedCrawlableFilter]);
-
-  const handleResultPage = (url: string) => {
-    router.push(`/results?url=${encodeURIComponent(url)}`);
-  };
-
-  // Pagination
-  const [page, setPage] = React.useState(1);
-  const [resultsPerPage, setResultsPerPage] = React.useState(5);
-  const pages = Math.ceil(filteredItems.length / resultsPerPage);
-
-  const handleResultsPerPageChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const newResultsPerPage = parseInt(event.target.value, 10);
-    setResultsPerPage(newResultsPerPage);
-    setPage(1);
-  };
-
-  // let items = React.useMemo(() => {
-  //   const start = (page - 1) * resultsPerPage;
-  //   const end = start + resultsPerPage;
-
-  //   return filteredItems.slice(start, end);
-  // }, [page, filteredItems, resultsPerPage]);
-
-  useEffect(() => {
-    console.log('urls length: ', urls.length);
-    if (urls && urls.length > 0 && urls.length !== results.length) {
-      urls.forEach((url) => {
-        if (!processedUrls.includes(url) && !processingUrls.includes(url)) {
-          // add to array of urls still being processed
-          processingUrls.push(url);
-          console.log('API call for:', url);
-          getScrapingResults(url);
-          processingUrls.splice(processingUrls.indexOf(url), 1);
-          processedUrls.push(url);
-        }
-      });
-    } else {
-      // allows to naviagte back to this page without rescraping the urls
-      if (processedUrls.length > 1) {
-        // Generate summary report
-        console.log('Results:', results);
-        const summary = generateSummary(results);
-        console.log('Summary:', summary);
-        setSummaryReport(summary);
-      }
-    }
-  }, [urls.length]);
-
-  useEffect(() => {
-    if (urls.length === results.length) {
-      // allows to navigate back to this page without rescraping the urls
-      setUrls([]);
-    }
-  }, [results]);
-
-  const getScrapingResults = async (url: string) => {
-    try {
-      const response = await fetch(
-        `http://localhost:3000/api/scraper?url=${encodeURIComponent(url)}`
-      );
-      const data = (await response.json()) as Scraping;
-      console.log('Response', data);
-      setResults((prevResults: Scraping[]) => [...prevResults, data]);
-    } catch (error) {
-      console.error('Error when scraping website:', error);
-    }
-  };
-
-  const onSearchChange = (value: string) => {
-    if (value) {
-      setSearchValue(value);
-      setPage(1);
-    } else {
-      setSearchValue('');
-    }
-  };
-
-  const handleStatusFilterChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const status = event.target.value;
-    setSelectedStatusFilter(status);
-  };
-
-  const handleCrawlableFilterChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const crawlable = event.target.value;
-    setSelectedCrawlableFilter(crawlable);
-  };
-
-  const handleSummaryPage = () => {
-    router.push(`/summaryreport`);
-  };
 
   // Mock data for demonstration
   const mockItems = [
@@ -187,11 +35,108 @@ function ResultsComponent() {
       timestamp: '2021-09-01 12:00:00',
       isSummary: true,
     },
+    {
+      reportName: 'Summary Report for https://example.com',
+      timestamp: '2021-09-01 12:00:00',
+      isSummary: true,
+    },
+    {
+      reportName: 'Report 2',
+      timestamp: '2021-09-01 12:00:00',
+      isSummary: false,
+    },
+    {
+      reportName: 'Report 3',
+      timestamp: '2021-09-01 12:00:00',
+      isSummary: false,
+    },
+    {
+      reportName: 'Report 4',
+      timestamp: '2021-09-01 12:00:00',
+      isSummary: false,
+    },
+    {
+      reportName: 'Summary Report 2',
+      timestamp: '2021-09-02 12:00:00',
+      isSummary: true,
+    },
+    {
+      reportName: 'Summary Report for https://example2.com',
+      timestamp: '2021-09-02 12:00:00',
+      isSummary: true,
+    },
+    {
+      reportName: 'Report 5',
+      timestamp: '2021-09-02 12:00:00',
+      isSummary: false,
+    },
+    {
+      reportName: 'Report 6',
+      timestamp: '2021-09-02 12:00:00',
+      isSummary: false,
+    },
+    {
+      reportName: 'Report 7',
+      timestamp: '2021-09-02 12:00:00',
+      isSummary: false,
+    },
+    {
+      reportName: 'Report 8',
+      timestamp: '2021-09-02 12:00:00',
+      isSummary: false,
+    },
     // Add more mock items as needed
   ];
 
-  // Use mockItems for initial display
-  const items = mockItems;
+  // split the items into two arrays
+  const reports = mockItems.filter(item => !item.isSummary);
+  const summaryReports = mockItems.filter(item => item.isSummary);
+
+
+  const handleResultPage = (url: string) => {
+    router.push(`/results?url=${encodeURIComponent(url)}`);
+  };
+
+  // Report pagination
+  const [page, setPage] = React.useState(1);
+  const [resultsPerPage, setResultsPerPage] = React.useState(5);
+  const pages = Math.ceil(reports.length / resultsPerPage);
+
+  const handleReportsPerPageChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const newResultsPerPage = parseInt(event.target.value, 10);
+    setResultsPerPage(newResultsPerPage);
+    setPage(1);
+  };
+
+  const splitReports = React.useMemo(() => {
+    const start = (page - 1) * resultsPerPage;
+    const end = start + resultsPerPage;
+    return reports.slice(start, end);
+  }, [page, resultsPerPage, reports]);
+
+  // Summary report pagination
+  const [summaryPage, setSummaryPage] = React.useState(1);
+  const [summaryResultsPerPage, setSummaryResultsPerPage] = React.useState(5);
+  const summaryPages = Math.ceil(summaryReports.length / summaryResultsPerPage);
+
+  const handleSummaryReportsPerPageChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const newResultsPerPage = parseInt(event.target.value, 10);
+    setSummaryResultsPerPage(newResultsPerPage);
+    setSummaryPage(1);
+  };
+
+  const splitSummaryReports = React.useMemo(() => {
+    const start = (summaryPage - 1) * summaryResultsPerPage;
+    const end = start + summaryResultsPerPage;
+    return summaryReports.slice(start, end);
+  }, [summaryPage, summaryResultsPerPage, summaryReports]);
+
+
+  
 
   return (
     <div className="p-4">
@@ -201,14 +146,14 @@ function ResultsComponent() {
 
       <div className="flex justify-between items-center mb-2">
         <span className="text-default-400 text-small">
-          Total {filteredItems.length} reports saved
+          Total {reports.length} reports saved
         </span>
         <label className="flex items-center text-default-400 text-small">
           Results per page:
           <select
             value={resultsPerPage}
             className="bg-transparent outline-none text-default-400 text-small"
-            onChange={handleResultsPerPageChange}
+            onChange={handleReportsPerPageChange}
             aria-label="Number of results per page"
           >
             <option value="2">2</option>
@@ -223,7 +168,7 @@ function ResultsComponent() {
         aria-label="Scrape result table"
         bottomContent={
           <>
-            {results.length > 0 && (
+            {reports.length > 0 && (
               <div className="flex w-full justify-center">
                 <WEEPagination
                   loop
@@ -258,9 +203,7 @@ function ResultsComponent() {
         </TableHeader>
 
         <TableBody emptyContent={'You have no saved reports'}>
-          {items
-            .filter(item => !item.isSummary)
-            .map((item, index) => (
+          {splitReports.map((item, index) => (
               <TableRow key={index}>
                 <TableCell>
                   <Link href={`/results?url=${encodeURIComponent(item.reportName)}`}>
@@ -290,14 +233,14 @@ function ResultsComponent() {
 
       <div className="flex justify-between items-center mb-2">
         <span className="text-default-400 text-small">
-          Total {filteredItems.length} saved summary reports
+          Total {summaryReports.length} saved summary reports
         </span>
         <label className="flex items-center text-default-400 text-small">
           Results per page:
           <select
-            value={resultsPerPage}
+            value={summaryResultsPerPage}
             className="bg-transparent outline-none text-default-400 text-small"
-            onChange={handleResultsPerPageChange}
+            onChange={handleSummaryReportsPerPageChange}
             aria-label="Number of results per page"
           >
             <option value="2">2</option>
@@ -312,15 +255,15 @@ function ResultsComponent() {
         aria-label="Scrape result table"
         bottomContent={
           <>
-            {results.length > 0 && (
+            {summaryReports.length > 0 && (
               <div className="flex w-full justify-center">
                 <WEEPagination
                   loop
                   showControls
                   color="stone"
-                  page={page}
-                  total={pages}
-                  onChange={(page) => setPage(page)}
+                  page={summaryPage}
+                  total={summaryPages}
+                  onChange={(page) => setSummaryPage(page)}
                   aria-label="Pagination"
                 />
               </div>
@@ -347,9 +290,7 @@ function ResultsComponent() {
         </TableHeader>
 
         <TableBody emptyContent={'You have no saved summary reports'}>
-        {items
-            .filter(item => item.isSummary)
-            .map((item, index) => (
+        {splitSummaryReports.map((item, index) => (
               <TableRow key={index}>
                 <TableCell>
                   <Link href={`/results?url=${encodeURIComponent(item.reportName)}`}>
