9cc92206615e0b3ad88702c9020894578c2b5bc0
MignonErasmus <u22492586@tuks.co.za>
2024-07-10 14:55:01 +0200

    added additional info

--- wee/frontend/src/app/(pages)/comparison/page.tsx
+++ wee/frontend/src/app/(pages)/comparison/page.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import WEETable from '../../components/Util/Table';
 import WEESelect from "../../components/Util/Select";
-import { Button, Chip, TableHeader, TableColumn, TableBody, TableRow, TableCell, SelectItem } from '@nextui-org/react';
+import { Image, Button, Chip, TableHeader, TableColumn, TableBody, TableRow, TableCell, SelectItem } from '@nextui-org/react';
 import { useScrapingContext } from '../../context/ScrapingContext';
 import { useRouter } from 'next/navigation';
 import Scraping from "../../models/ScrapingModel";
@@ -11,6 +11,7 @@ export default function ComparisonReport() {
     const { results } = useScrapingContext();
     const router = useRouter();
     const [websiteOne, setWebsiteOne] = React.useState<Scraping>();
+    const [websiteTwo, setWebsiteTwo] = React.useState<Scraping>();
 
     useEffect(() => {
 
@@ -22,8 +23,12 @@ export default function ComparisonReport() {
 
     const handleWebsiteOne = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const website1: number = parseInt(event.target.value, 10);
-        console.log(website1);
         setWebsiteOne(results[website1]);
+    }
+
+    const handleWebsiteTwo = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const website2: number = parseInt(event.target.value, 10);
+        setWebsiteTwo(results[website2]);
     }
 
     return (
@@ -54,6 +59,7 @@ export default function ComparisonReport() {
                 <WEESelect
                     label="Website 2"
                     className="w-1/2 pl-3 pb-3"
+                    onChange={handleWebsiteTwo}
                 >
                     {results.map((item, index) => (
                         <SelectItem key={index}>{item.url}</SelectItem>
@@ -69,15 +75,58 @@ export default function ComparisonReport() {
                 <TableBody>                  
                     <TableRow key="1">
                         <TableCell>{websiteOne ? websiteOne.url : 'Url'}</TableCell>
-                        <TableCell>abc</TableCell>
+                        <TableCell>{websiteTwo ? websiteTwo.url : 'Url'}</TableCell>
                     </TableRow>
                     <TableRow key="2">
-                        <TableCell>{websiteOne ? websiteOne.domainStatus : 'Domain Status'}</TableCell>
-                        <TableCell>abc</TableCell>
+                        <TableCell>
+                            {websiteOne 
+                            ? 
+                            <>
+                                <Chip
+                                    radius="sm"
+                                    color={websiteOne && websiteOne.domainStatus === 'live' ? 'success' : 'warning'}
+                                    variant="flat"
+                                >
+                                    {websiteOne && websiteOne.domainStatus === 'live' ? 'Live' : 'Parked'}
+                                </Chip>
+                            </>
+                            : 'Domain Status'}
+                        </TableCell>
+                        <TableCell>
+                            {websiteTwo 
+                            ? 
+                            <>
+                                <Chip
+                                    radius="sm"
+                                    color={websiteTwo && websiteTwo.domainStatus === 'live' ? 'success' : 'warning'}
+                                    variant="flat"
+                                >
+                                    {websiteTwo && websiteTwo.domainStatus === 'live' ? 'Live' : 'Parked'}
+                                </Chip>
+                            </>
+                            : 'Domain Status'}
+                        </TableCell>
                     </TableRow>
                     <TableRow key="3">
-                        <TableCell>abc</TableCell>
-                        <TableCell>abc</TableCell>
+                        <TableCell>
+                            {/* <div className="flex justify-center"> */}
+                                {/* <div className="flex justify-center"> */}
+                                    <Image
+                                        alt="Logo"
+                                        src={websiteOne?.logo}
+                                        className="centered-image max-h-48 shadow-md shadow-zinc-150 dark:shadow-zinc-900"
+                                    />
+                                {/* </div> */}
+                            {/* </div> */}
+                    
+                        </TableCell>
+                        <TableCell>
+                            <Image
+                                alt="Logo"
+                                src={websiteTwo?.logo}
+                                className="centered-image max-h-48 shadow-md shadow-zinc-150 dark:shadow-zinc-900"
+                            />
+                        </TableCell>
                     </TableRow>
                     <TableRow key="4">
                         <TableCell>abc</TableCell>
