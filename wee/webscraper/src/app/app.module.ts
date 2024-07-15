4ca61917c6a351b4079cd812505a66706d9c71b5
CaitMS <caitymsimon@gmail.com>
2024-07-15 10:38:54 +0200

    Updated file structure

--- wee/webscraper/src/app/app.module.ts
+++ wee/webscraper/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ScraperModule } from '../scraper/scraper.module';
+import { ScraperModule } from '../scraper.module';
 
 @Module({
   imports: [ScraperModule],
