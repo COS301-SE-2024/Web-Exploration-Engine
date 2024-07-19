b0f2e8f5c9110bd2b10ce295eba47ba943b415b9
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-07-19 16:16:40 +0200

    logs for all errors

--- wee/webscraper/src/robots/robots.service.ts
+++ wee/webscraper/src/robots/robots.service.ts
@@ -1,15 +1,21 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, Logger, HttpException, HttpStatus } from '@nestjs/common';
 import { ErrorResponse } from '../models/ServiceModels';
 import { RobotsResponse } from '../models/ServiceModels';
 
 import fetch from 'node-fetch';
 
-
 @Injectable()
 export class RobotsService {
   // Returns all the paths user agent can scrape in the form of an array
-  async extractAllowedPaths(baseUrl: string): 
-  Promise<{allowedPaths: string[], disallowedPaths: string[]}> {
+  logger: Logger;
+
+  constructor() {
+    this.logger = new Logger('ROBOTS');
+  }
+
+  async extractAllowedPaths(
+    baseUrl: string
+  ): Promise<{ allowedPaths: string[]; disallowedPaths: string[] }> {
     // Extract base URL
     const domain = this.extractDomain(baseUrl);
     // Construct the URL for the robots.txt file
@@ -21,6 +27,7 @@ export class RobotsService {
       // Check if website has a robots.txt file -- if not, return an empty set
       if (response.status === 404) {
         console.warn(`robots.txt does not exist for ${robotstxtUrl}`);
+        this.logger.warn(`robots.txt does not exist for ${robotstxtUrl}`);
         return {
           allowedPaths: [],
           disallowedPaths: [],
@@ -29,13 +36,21 @@ export class RobotsService {
 
       // Check if error occured
       if (!response.ok) {
-        throw new Error(`An error occurred while fetching robots.txt from ${robotstxtUrl}`);
+        this.logger.error(
+          `An error occurred while fetching robots.txt from ${robotstxtUrl}`
+        );
+
+        throw new Error(
+          `An error occurred while fetching robots.txt from ${robotstxtUrl}`
+        );
       }
 
       // Parse the robots.txt file
       const robotstxt = await response.text();
+
       if (!robotstxt) {
         console.warn(`robots.txt content is empty for ${robotstxtUrl}`);
+        this.logger.warn(`robots.txt content is empty for ${robotstxtUrl}`);
         return {
           allowedPaths: [],
           disallowedPaths: [],
@@ -53,12 +68,18 @@ export class RobotsService {
         if (line.startsWith('User-agent:')) {
           const userAgent = line.substring(11).trim();
           isGlobalUserAgent = userAgent === '*';
-        } else if ((line.startsWith('Disallow:') || line.startsWith('disallow:')) && isGlobalUserAgent) {
+        } else if (
+          (line.startsWith('Disallow:') || line.startsWith('disallow:')) &&
+          isGlobalUserAgent
+        ) {
           const path = line.substring(9).trim();
           if (path.startsWith('/')) {
             disallowedPaths.add(path);
           }
-        } else if ((line.startsWith('Allow:') || line.startsWith('allow:')) && isGlobalUserAgent) {
+        } else if (
+          (line.startsWith('Allow:') || line.startsWith('allow:')) &&
+          isGlobalUserAgent
+        ) {
           const path = line.substring(6).trim();
           if (path.startsWith('/')) {
             allowedPaths.add(path);
@@ -66,7 +87,10 @@ export class RobotsService {
         }
       });
 
-      return {allowedPaths: Array.from(allowedPaths), disallowedPaths: Array.from(disallowedPaths)};
+      return {
+        allowedPaths: Array.from(allowedPaths),
+        disallowedPaths: Array.from(disallowedPaths),
+      };
     } catch (error) {
       throw new Error('An error occurred while interpreting robots.txt file');
     }
@@ -75,50 +99,60 @@ export class RobotsService {
   extractDomain(url: string): string {
     try {
       const parsedUrl = new URL(url);
+      this.logger.log(`Parse url ${parsedUrl}`);
+
       return parsedUrl.origin;
     } catch (error) {
+      this.logger.error(`Extracting Domain Error ${error}`);
       throw new Error('Invalid URL');
     }
   }
 
-  isCrawlingAllowed(url: string, disallowedPaths: string[], allowedPaths: string[]): boolean {
+  isCrawlingAllowed(
+    url: string,
+    disallowedPaths: string[],
+    allowedPaths: string[]
+  ): boolean {
     try {
-        const urlObject = new URL(url);
-        const path = urlObject.pathname;
+      const urlObject = new URL(url);
+      const path = urlObject.pathname;
 
-        // Function to convert paths with wildcards to regex
-        const pathToRegex = (path: string) => {
-            const escapedPath = path.replace(/[.+?^${}()|[\]\\]/g, '\\$&').replace(/\*/g, '.*');
-            return new RegExp(`^${escapedPath}$`);
-        };
+      // Function to convert paths with wildcards to regex
+      const pathToRegex = (path: string) => {
+        const escapedPath = path
+          .replace(/[.+?^${}()|[\]\\]/g, '\\$&')
+          .replace(/\*/g, '.*');
+        return new RegExp(`^${escapedPath}$`);
+      };
 
-        // Check against disallowed paths
-        for (const disallowedPath of disallowedPaths) {
-            const regex = pathToRegex(disallowedPath);
-            if (regex.test(path)) {
-                return false;
-            }
+      // Check against disallowed paths
+      for (const disallowedPath of disallowedPaths) {
+        const regex = pathToRegex(disallowedPath);
+        if (regex.test(path)) {
+          return false;
         }
+      }
 
-        // Check against allowed paths
-        for (const allowedPath of allowedPaths) {
-            const regex = pathToRegex(allowedPath);
-            if (regex.test(path)) {
-                return true;
-            }
+      // Check against allowed paths
+      for (const allowedPath of allowedPaths) {
+        const regex = pathToRegex(allowedPath);
+        if (regex.test(path)) {
+          return true;
         }
+      }
 
-        // if not allowed or disallowed, default to allowed
-        return true;
+      // if not allowed or disallowed, default to allowed
+      return true;
     } catch (error) {
-        if (error.message.includes("Invalid URL")) {
-            throw new Error(`Invalid URL: ${url}`);
-        } else {
-            throw new Error(`An error occurred while checking if crawling is allowed: ${error.message}`);
-        }
+      if (error.message.includes('Invalid URL')) {
+        throw new Error(`Invalid URL: ${url}`);
+      } else {
+        throw new Error(
+          `An error occurred while checking if crawling is allowed: ${error.message}`
+        );
+      }
     }
   }
-  
 
   isRootPathAllowed(disallowedPaths: Set<string>): boolean {
     return !disallowedPaths.has('/');
@@ -130,15 +164,21 @@ export class RobotsService {
       return {
         errorStatus: 400,
         errorCode: '400 Bad Request',
-        errorMessage: 'URL parameter is required'
+        errorMessage: 'URL parameter is required',
       } as ErrorResponse;
     }
 
     try {
       const baseUrl = this.extractDomain(url);
-      const {allowedPaths, disallowedPaths} = await this.extractAllowedPaths(baseUrl);
+      const { allowedPaths, disallowedPaths } = await this.extractAllowedPaths(
+        baseUrl
+      );
       const isBaseUrlAllowed = this.isRootPathAllowed(new Set(disallowedPaths));
-      const isUrlScrapable = this.isCrawlingAllowed(url, disallowedPaths, allowedPaths);
+      const isUrlScrapable = this.isCrawlingAllowed(
+        url,
+        disallowedPaths,
+        allowedPaths
+      );
       return {
         baseUrl,
         allowedPaths,
@@ -155,5 +195,4 @@ export class RobotsService {
       } as ErrorResponse;
     }
   }
-
 }
