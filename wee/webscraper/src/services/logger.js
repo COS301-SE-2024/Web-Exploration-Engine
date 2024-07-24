abd8f1b683769f4ce122f25f197c8396b0c3d1aa
Siyamthanda Ndlovu <99127918+siyamthandandlovu@users.noreply.github.com>
2024-07-24 03:06:12 +0000

    prev logger updates

--- wee/webscraper/src/services/logger.js
+++ wee/webscraper/src/services/logger.js
@@ -1,5 +1,6 @@
 import { createLogger, format, transports } from 'winston';
 import 'winston-daily-rotate-file';
+const { combine, timestamp, json } = winston.format;
 
 const getLogger = (fileName = 'application') => {
   const fileLogTransport = new transports.DailyRotateFile({
@@ -7,15 +8,15 @@ const getLogger = (fileName = 'application') => {
     datePattern: 'YYYY-MM-DD',
     zippedArchive: true,
     maxSize: '20m',
-    maxFiles: '30d',
+    maxFiles: '100d',
   });
 
   const consoleTransport = new transports.Console({
     //level: process.env.LOG_LEVEL,
     handleExceptions: false,
-    json: false,
+    json: true,
     colorize: true,
-    format: format.printf((i) => `${i.message}`),
+    format: format.printf((i) => `${i.message} ${i.servicename} `),
   });
 
   const logger = createLogger({
@@ -27,8 +28,8 @@ const getLogger = (fileName = 'application') => {
       format.errors({ stack: true }),
       format.splat(),
       format.printf(
-        ({ level, message, label = process.env.NODE_ENV, timestamp }) =>
-          `${timestamp} [${label}] ${level}: ${message}`
+        ({ level, message, servicename, label = process.env.NODE_ENV, timestamp }) =>
+          `${timestamp} [${label}] ${level}: ${message},${servicename}`
       )
     ),
     defaultMeta: { service: 'my-app' },
