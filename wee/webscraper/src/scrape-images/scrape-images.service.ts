a3bb9881330dd1eb06692dcfcfbe5ba93fe0e487
Siyamthanda Ndlovu <99127918+siyamthandandlovu@users.noreply.github.com>
2024-07-26 16:49:04 +0000

    log updates

--- wee/webscraper/src/scrape-images/scrape-images.service.ts
+++ wee/webscraper/src/scrape-images/scrape-images.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import * as puppeteer from 'puppeteer';
 import { RobotsResponse } from '../models/ServiceModels';
+import logger from '../../../services/webscraperlogger';
 
+const serviceName = "ScrapeImagesService";
 
 @Injectable()
 export class ScrapeImagesService {
@@ -11,9 +13,12 @@ export class ScrapeImagesService {
      * @returns {Promise<string[]>} - Returns a promise that resolves to an array of image URLs.
      */
    async scrapeImages(url: string, robots: RobotsResponse): Promise<string[]> {
+    logger.log(serviceName);
+    
     // Possible improvement: scrape current URL first, if no images found, scrape root URL
     // Check if the URL is allowed to be scraped
     if (!robots.isUrlScrapable) {
+        logger.warn('${serviceName} Crawling not allowed for this URL');
         console.error('Crawling not allowed for this URL');
         return [];
     }
@@ -28,6 +33,7 @@ export class ScrapeImagesService {
         await browser.close();
         return imageUrls.slice(0, 50);
     } catch (error) {
+      logger.error(`${serviceName} Failed to scrape images: ${error.message}`);
       console.error(`Failed to scrape images: ${error.message}`);
         return [];
     }
