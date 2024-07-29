26c2398f1a9cca1e7704cb1598e4042cf223d957
CaitMS <caitymsimon@gmail.com>
2024-07-29 15:55:29 +0200

    Added additional integration tests for new endpoints

--- wee/webscraper/src/integration.test.ts
+++ wee/webscraper/src/integration.test.ts
@@ -4,8 +4,6 @@ import request from 'supertest';
 import { ScraperModule } from './scraper.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PubSubService } from './pub-sub/pub_sub.service';
-import { ScraperService } from './scraper.service';
-import { publish } from 'rxjs';
 
 jest.mock('ioredis', () => {
   class MockRedis {}
@@ -66,7 +64,7 @@ describe('ScraperController', () => {
   }, 60000);
 
   describe('/GET scraper', () => {
-    it('should scrape a website successfully', async () => {
+    it('should publsih a scrape task successfully', async () => {
       const url = 'https://example.com';
       
       // Make a request to the scraper endpoint
@@ -81,18 +79,406 @@ describe('ScraperController', () => {
       expect(response.body).toHaveProperty('message');
       expect(response.body).toHaveProperty('status');
       expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/scrape\//); 
+    });
 
-      // Extract the polling URL from the response
-      const pollingUrl = response.body.pollingUrl;
-      
-      // Validate that the polling URL is a valid URL
-      expect(pollingUrl).toBeDefined();
-      expect(pollingUrl).toMatch(/scraper\//); // Ensure it starts with http:// or https://
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper')
+        .expect(HttpStatus.BAD_REQUEST);
 
-      // Make a request to the polling URL
-      await request(app.getHttpServer())
-        .get(pollingUrl)
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/read-robots', () => {
+    it('should publish a read-robots task successuly', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/read-robots?url=' + encodeURIComponent(url))
+        .expect(HttpStatus.OK)
+        .expect('Content-Type', /json/);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/read-robots\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/read-robots')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/read-robots?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/scrape-metadata', () => {
+    it('should scrape metadata successfully', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-metadata?url=' + encodeURIComponent(url))
+        .expect(HttpStatus.OK)
+        .expect('Content-Type', /json/);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/scrape-metadata\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-metadata')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-metadata?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/scrape-status', () => {
+    it('should return status successfully', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-status?url=' + encodeURIComponent(url))
         .expect(HttpStatus.OK);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/scrape-status\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-status')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-status?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/classify-industry', () => {
+    it('should classify industry successfully', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/classify-industry?url=' + encodeURIComponent(url))
+        .expect(HttpStatus.OK)
+        .expect('Content-Type', /json/);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/classify-industry\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/classify-industry')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/classify-industry?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/scrape-images', () => {
+    it('should scrape images successfully', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-images?url=' + encodeURIComponent(url))
+        .expect(HttpStatus.OK)
+        .expect('Content-Type', /json/);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/scrape-images\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-images')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-images?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/scrape-logo', () => {
+    it('should scrape logo successfully', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-logo?url=' + encodeURIComponent(url))
+        .expect(HttpStatus.OK)
+        .expect('Content-Type', /json/);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/scrape-logo\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-logo')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-logo?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/screenshot', () => {
+    it('should get screenshot successfully', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/screenshot?url=' + encodeURIComponent(url))
+        .expect(HttpStatus.OK)
+        .expect('Content-Type', /json/);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/screenshot\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/screenshot')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/screenshot?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/scrape-contact-info', () => {
+    it('should scrape contact info successfully', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-contact-info?url=' + encodeURIComponent(url))
+        .expect(HttpStatus.OK)
+        .expect('Content-Type', /json/);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/scrape-contact-info\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-contact-info')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-contact-info?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/scrape-addresses', () => {
+    it('should scrape addresses successfully', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-addresses?url=' + encodeURIComponent(url))
+        .expect(HttpStatus.OK)
+        .expect('Content-Type', /json/);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/scrape-addresses\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-addresses')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/scrape-addresses?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
+    });
+  });
+
+  describe('/GET scraper/seo-analysis', () => {
+    it('should perform SEO analysis successfully', async () => {
+      const url = 'https://example.com';
+      const response = await request(app.getHttpServer())
+        .get('/scraper/seo-analysis?url=' + encodeURIComponent(url))
+        .expect(HttpStatus.OK)
+        .expect('Content-Type', /json/);
+
+      expect(response.body).toBeDefined();
+
+      // Check that the response contains the expected properties
+      expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('status');
+      expect(response.body).toHaveProperty('pollingUrl');
+      expect(response.body.pollingUrl).toMatch(/status\/seo-analysis\//);
+    });
+
+    it('should return an error if no URL is provided', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/seo-analysis')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('URL is required');
+    });
+
+    it('should return an error if the URL is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/scraper/seo-analysis?url=invalid-url')
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body).toBeDefined();
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toEqual('Invalid URL format');
     });
   });
 
