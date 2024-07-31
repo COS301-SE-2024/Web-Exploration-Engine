fac0f86ca285ed5e9202b92cecd4365b25fb6df7
katnicole14 <u22543946@tuks.co.za>
2024-07-31 14:07:27 +0200

    should return positive and negative words

--- wee/webscraper/src/sentiment-analysis/sentiment-analysis.spec.ts
+++ wee/webscraper/src/sentiment-analysis/sentiment-analysis.spec.ts
@@ -70,16 +70,80 @@ describe('SentimentAnalysisService', () => {
         ogImage: 'https://example.com/image.jpg',
       };
 
-      // Mocking an unexpected API response format
       mockedAxios.post.mockResolvedValueOnce({
         data: [{ label: 'joy', score: 0.8 }, { label: 'anger', score: 0.2 }],
       });
 
       const result = await service.analyzeEmotions(metadata);
 
-      // Expecting empty object due to unexpected format
       expect(result).toEqual({});
     });
   });
 
+  describe('getPositiveNegativeWords', () => {
+    it('should return positive and negative words', async () => {
+      const metadata: Metadata = {
+        title: 'Takealot.com: Online Shopping | SA\'s leading online store',
+        description: 'South Africa\'s leading online store. Fast, reliable delivery to your door. Many ways to pay. Shop anything you can imagine: TVs, laptops, cellphones, kitchen appliances, toys, books, beauty & more. Shop the mobile app anytime, anywhere.',
+        keywords: null,
+        ogTitle: 'Takealot.com: Online Shopping | SA\'s leading online store',
+        ogDescription: 'South Africa\'s leading online store. Fast, reliable delivery to your door. Many ways to pay. Shop anything you can imagine: TVs, laptops, cellphones, kitchen appliances, toys, books, beauty & more. Shop the mobile app anytime, anywhere.',
+        ogImage: 'https://www.takealot.com/static/images/logo_transparent.png'
+      };
+
+
+      mockedAxios.post.mockResolvedValueOnce({
+        data: [
+     //come back to this part and fix it ??
+        ],
+      });
+
+      const result = await service.getPositiveNegativeWords(metadata);
+
+      expect(result).toEqual({
+        positiveWords: [],
+        negativeWords: [],
+      });
+    });
+
+
+    it('should handle errors and return empty word lists', async () => {
+      const metadata: Metadata = {
+        title: '',
+        description: '',
+        keywords: '',
+        ogTitle: '',
+        ogDescription: '',
+        ogImage: '',
+      };
+
+      mockedAxios.post.mockRejectedValueOnce(new Error('API error'));
+
+      const result = await service.getPositiveNegativeWords(metadata);
+
+      expect(result).toEqual({ positiveWords: [], negativeWords: [] });
+    });
+
+    it('should handle unexpected API response format', async () => {
+      const metadata: Metadata = {
+        title: 'Sample Title',
+        description: 'Sample Description',
+        keywords: 'Sample Keywords',
+        ogTitle: 'Open Graph Title',
+        ogDescription: 'Open Graph Description',
+        ogImage: 'https://example.com/image.jpg',
+      };
+
+      mockedAxios.post.mockResolvedValueOnce({
+        data: [{ label: 'unexpectedLabel', score: 0.5 }],
+      });
+
+      const result = await service.getPositiveNegativeWords(metadata);
+
+      expect(result).toEqual({
+        positiveWords: [],
+        negativeWords: [],
+      });
+    });
+  });
 });
