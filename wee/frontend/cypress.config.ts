16401a9eda1d59a26aa01fcb0ded45f0dc0d24f1
CaitMS <caitymsimon@gmail.com>
2024-07-31 19:10:40 +0200

    Revert "change timeout"
    This reverts commit 5c1a5027c2eb67049880d21bd89b2d766d4743bb.

--- wee/frontend/cypress.config.ts
+++ wee/frontend/cypress.config.ts
@@ -3,7 +3,6 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  pageLoadTimeout:10000,
   e2e: {
     ...nxE2EPreset(__filename, { cypressDir: 'cypress' }),
     baseUrl: 'http://localhost:3000',
