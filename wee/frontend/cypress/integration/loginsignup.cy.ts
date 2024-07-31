5c1a5027c2eb67049880d21bd89b2d766d4743bb
Siyamthanda Ndlovu <99127918+siyamthandandlovu@users.noreply.github.com>
2024-07-31 16:24:38 +0000

    change timeout

--- wee/frontend/cypress/integration/loginsignup.cy.ts
+++ wee/frontend/cypress/integration/loginsignup.cy.ts
@@ -2,13 +2,52 @@ describe('signup', () => {
   it('should be on right page', () => {
     cy.visit('/signup');
     cy.contains(/Become a Member/i).should('exist');
+
+    
+    cy.log("Testing Text Content")
+    cy.contains(/First name/i).should('exist');
+    cy.contains(/Last name/i).should('exist');
+    cy.contains(/email/i).should('exist');
+    cy.contains(/password/i).should('exist');
+    
+    cy.log("Testing Buttons");
+    cy.get('[data-testid="btn-create-account"]').should("exist");
+    
+    cy.log("Testing Inputs");
+    cy.get('[data-testid="input-first-name"]').should("exist")
+   /*  cy.get('[data-testid="input-first-name"]').type("Amandla");
+    cy.contains(/amandla/i).should('exist'); */
+
+    cy.get('[data-testid="input-last-name"]').should("exist");
+    cy.get('[data-testid="input-email"]').should("exist");
+    cy.get('[data-testid="input-password"]').should("exist");
+    
+    cy.log("Testing Links");
+    cy.get('[data-testid="link-login"]').should("exist");
+
   });
 });
 
 describe('login', () => {
   it('should be on right page', () => {
     cy.visit('/login');
+    
+    cy.log("Tesing Text Content");
     cy.contains(/Ready to Dive Back In/i).should('exist');
+    cy.contains(/Email/i).should('exist');
+    cy.contains(/password/i).should('exist');
+    cy.contains(/google/i).should('exist');
+
+    cy.log("Testing Buttons")
+    cy.get('[data-testid="btn-login"]').should("exist");
+
+
+    cy.log("Testing Inputs")
+    cy.get('[data-testid="input-email"]').should("exist");
+    cy.get('[data-testid="input-password"]').should("exist");
+
+
+
   });
 });
 
