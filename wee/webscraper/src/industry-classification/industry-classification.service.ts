6e89a41e96bcaa602fb64ea7994c9dfefe8e1393
Siyamthanda Ndlovu <99127918+siyamthandandlovu@users.noreply.github.com>
2024-08-05 16:27:09 +0000

    merge conflict resolved

--- wee/webscraper/src/industry-classification/industry-classification.service.ts
+++ wee/webscraper/src/industry-classification/industry-classification.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { IndustryClassification, Metadata } from '../models/ServiceModels';
-import axios from 'axios';
 import logger from '../../../services/webscraperlogger';
-
+import axios from 'axios';
 const serviceName = "[IndustryClassificationService]";
 
 @Injectable()
 export class IndustryClassificationService {
+
   private readonly HUGGING_FACE_API_URL =
     'https://api-inference.huggingface.co/models/sampathkethineedi/industry-classification-api';
 
@@ -28,9 +28,10 @@ export class IndustryClassificationService {
     'Insurance','Legal Services','Fitness and Wellness','Jewelry','Entertainment and Recreation'
   ];
 
+ 
   async classifyIndustry(url: string, metadata: Metadata): Promise<IndustryClassification> {
-
     logger.debug(`${serviceName}`);
+
     try {
       const metadataClass = await this.metadataClassify(metadata);
       //console.log('Metadata Classification:', metadataClass);
@@ -41,10 +42,12 @@ export class IndustryClassificationService {
       const zeroShotDomainClassify = await this.zeroShotDomainClassify(url);
       //console.log('Zero-Shot Domain Classification:', zeroShotDomainClassify);
       return { metadataClass, domainClass, zeroShotMetaDataClassify , zeroShotDomainClassify };
-    }
+    } 
     catch (error) {
-      logger.error(`${serviceName}, Error with metadata class ${error.message}`)
-      return { 
+      logger.error(`${serviceName} ${error.message}`);
+
+      console.log(error.message);
+      return {
         metadataClass: {
           label: 'Unknown',
           score: 0,
@@ -100,7 +103,6 @@ export class IndustryClassificationService {
     } catch (error) {
       logger.error(`${serviceName} Error classifying industry: ${error.message}`);
       throw new Error(`Error classifying industry: ${error.message}`);
-
     }
   }
 
@@ -132,7 +134,6 @@ export class IndustryClassificationService {
         return res;
       } else {
         logger.error(`${serviceName} Failed to classify industry using Hugging Face model`);
-
         throw new Error('Failed to classify industry using Hugging Face model');
       }
     } catch (error) {
@@ -192,62 +193,7 @@ export class IndustryClassificationService {
   
       return topResults;
     } catch (error) {
-      logger.error(`${serviceName} Error classifying industry: ${error.message}`);
-      throw new Error(`Error classifying industry: ${error.message}`);
-    }
-  }
-
-  async zeroShotMetaDataClassify(metadata: Metadata): Promise<{label: string, score: number}[]> {
-    if (!metadata.title && !metadata.description && !metadata.keywords) {
-      return [
-        { label: 'Unknown', score: 0 },
-        { label: 'Unknown', score: 0 },
-        { label: 'Unknown', score: 0 },
-      ];
-    }
-  
-    const inputText = `${metadata.title} ${metadata.description} ${metadata.keywords}`;
-    const batches = this.createLabelBatches(this.CANDIDATE_LABELS, 10);
-  
-    try {
-      const allResults = [];
-  
-      for (const batch of batches) {
-        const response = await axios.post(
-          this.HUGGING_FACE_ZERO_SHOT_API_URL,
-          {
-            inputs: inputText,
-            parameters: { candidate_labels: batch }
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${this.HUGGING_FACE_API_TOKEN}`,
-            },
-          }
-        );
-
-        console.log('Response:', response);
-  
-        if (response.data && response.data.labels && response.data.scores) {
-          const results = response.data.labels.map((label: string, index: number) => ({
-            label,
-            score: response.data.scores[index]
-          }));
-          allResults.push(...results);
-        }
-  
-        //console.log('Batch results:', allResults);
-      }
-  
-      // Determine the top 3 
-      const topResults = allResults
-        .sort((a, b) => b.score - a.score)
-        .slice(0, 3);
-  
-      //console.log('Top 3 results:', topResults);
-  
-      return topResults;
-    } catch (error) {
+      logger.debug(`${serviceName} Error classifying industry: ${error.message}`);
       throw new Error(`Error classifying industry: ${error.message}`);
     }
   }
@@ -299,6 +245,7 @@ export class IndustryClassificationService {
   
       return topResults;
     } catch (error) {
+      logger.error(`${serviceName} Error classifying industry: ${error.message}`);      
       throw new Error(`Error classifying domain: ${error.message}`);
     }
   }
