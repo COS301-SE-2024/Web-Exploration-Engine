1414ec8d98062d7d7bb24338d344794193f7ab65
MignonErasmus <u22492586@tuks.co.za>
2024-08-08 16:25:34 +0200

    [skip ci] Fixed failing tests in frontend and api-service

--- wee/frontend/specs/services/pubsub.spec.tsx
+++ wee/frontend/specs/services/pubsub.spec.tsx
@@ -19,7 +19,7 @@ describe('checkJobStatus', () => {
 
     const data = await checkJobStatus('http://example.com');
     expect(data).toEqual(mockData);
-    expect(fetch).toHaveBeenCalledWith('http://localhost:3000/api/scraper/status/scrape/http%3A%2F%2Fexample.com');
+    expect(fetch).toHaveBeenCalledWith('http://localhost:3002/api/scraper/status?type=scrape&url=http%3A%2F%2Fexample.com');
   });
 
   it('should throw an error when the fetch response is not ok', async () => {
@@ -29,7 +29,7 @@ describe('checkJobStatus', () => {
     });
 
     await expect(checkJobStatus('http://example.com')).rejects.toThrow('Error fetching job status: Internal Server Error');
-    expect(fetch).toHaveBeenCalledWith('http://localhost:3000/api/scraper/status/scrape/http%3A%2F%2Fexample.com');
+    expect(fetch).toHaveBeenCalledWith('http://localhost:3002/api/scraper/status?type=scrape&url=http%3A%2F%2Fexample.com');
   });
 
   it('should throw an error when the fetch fails', async () => {
@@ -37,7 +37,7 @@ describe('checkJobStatus', () => {
     fetch.mockRejectedValue(new Error(errorMessage));
 
     await expect(checkJobStatus('http://example.com')).rejects.toThrow('Network Error');
-    expect(fetch).toHaveBeenCalledWith('http://localhost:3000/api/scraper/status/scrape/http%3A%2F%2Fexample.com');
+    expect(fetch).toHaveBeenCalledWith('http://localhost:3002/api/scraper/status?type=scrape&url=http%3A%2F%2Fexample.com');
   });
 });
 
