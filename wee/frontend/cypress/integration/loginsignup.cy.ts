fedbc599f6d6774e7653b25f623aa451249d5753
katnicole14 <u22543946@tuks.co.za>
2024-08-09 18:32:06 +0200

    rearranged tests

--- wee/frontend/cypress/integration/loginsignup.cy.ts
+++ wee/frontend/cypress/integration/loginsignup.cy.ts
@@ -1,15 +1,23 @@
-describe('signup', () => {
-  it('should be on right page', () => {
-    cy.visit('/signup');
-    cy.contains(/Become a Member/i).should('exist');
-  });
-});
 
-describe('login', () => {
-  it('should be on right page', () => {
-    cy.visit('/login');
-    cy.contains(/Ready to Dive Back In/i).should('exist');
+
+describe('Signup and Login Tests', () => {
+
+  describe('Signup Tests', () => {
+    it('should be on the right page', () => {
+      cy.visit('/signup');
+      cy.contains(/Become a Member/i).should('exist');
+    });
   });
+
+  describe('Login Page Tests', () => {
+    beforeEach(() => {
+      cy.visit('/login');
+    });
+
+    it('should be on the right page', () => {
+      cy.contains(/Ready to Dive Back In/i).should('exist');
+    });
+});
 });
 
 
