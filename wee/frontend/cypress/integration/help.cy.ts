8ee06c6acb50b2808c4d7106a9612788ee9980ae
katnicole14 <u22543946@tuks.co.za>
2024-08-09 17:19:48 +0200

    correct links

--- wee/frontend/cypress/integration/help.cy.ts
+++ wee/frontend/cypress/integration/help.cy.ts
@@ -9,12 +9,20 @@ describe('Help Page Tests', () => {
   it('help page should be accessible', () => {
     cy.testHelp('/help');
   });
-  
+
   it('should load the help page successfully with correct content', () => {
     cy.visit('/help');
 
     cy.get('#faq').should('be.visible');
     cy.get('#faq h1').should('contain.text', 'Frequently Asked Questions');
     cy.get('#faq h3').should('contain.text', 'How can we help you?');
+  });
+
+  it('should display video tutorials with correct links', () => {
+    cy.visit('/help');
+
+
+    cy.get('a[href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs"]').should('exist');
+    cy.get('a[href="https://www.youtube.com/watch?v=ZKcuvdnVF80&pp=ygURYSBiaXJkcyBsYXN0IGxvb2s%3D"]').should('exist');
   });
 });
