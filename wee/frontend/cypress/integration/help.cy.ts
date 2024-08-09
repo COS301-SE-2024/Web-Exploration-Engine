c992fb26648d9fee54d234c58bf299806aa07cc7
katnicole14 <u22543946@tuks.co.za>
2024-08-09 16:44:32 +0200

    correct content test

--- wee/frontend/cypress/integration/help.cy.ts
+++ wee/frontend/cypress/integration/help.cy.ts
@@ -9,4 +9,12 @@ describe('Help Page Tests', () => {
   it('help page should be accessible', () => {
     cy.testHelp('/help');
   });
+  
+  it('should load the help page successfully with correct content', () => {
+    cy.visit('/help');
+
+    cy.get('#faq').should('be.visible');
+    cy.get('#faq h1').should('contain.text', 'Frequently Asked Questions');
+    cy.get('#faq h3').should('contain.text', 'How can we help you?');
+  });
 });
