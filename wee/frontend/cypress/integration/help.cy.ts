23c8e7bea349725bf38150cf78345f1ef75ab3a9
katnicole14 <u22543946@tuks.co.za>
2024-08-09 16:43:48 +0200

    modifications

--- wee/frontend/cypress/integration/help.cy.ts
+++ wee/frontend/cypress/integration/help.cy.ts
@@ -1,11 +1,12 @@
-describe('help page', () => {
-  it('should be on right page', () => {
+describe('Help Page Tests', () => {
+
+  it('should be on the right page and contain key elements', () => {
     cy.visit('/help');
-    cy.contains(/Frequently asked questions/i).should('exist');
+    cy.contains(/Frequently Asked Questions/i).should('exist');
     cy.contains(/Feedback/i).should('exist');
   });
 
-  it('help should be available', () => {
+  it('help page should be accessible', () => {
     cy.testHelp('/help');
   });
 });
