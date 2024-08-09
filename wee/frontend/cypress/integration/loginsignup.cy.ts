887f422dc7c141cbd78607335097769dca9f80a8
katnicole14 <u22543946@tuks.co.za>
2024-08-09 18:34:42 +0200

    invalid input tests

--- wee/frontend/cypress/integration/loginsignup.cy.ts
+++ wee/frontend/cypress/integration/loginsignup.cy.ts
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/unsafe-to-chain-command */
 
 
 describe('Signup and Login Tests', () => {
@@ -17,7 +18,22 @@ describe('Signup and Login Tests', () => {
     it('should be on the right page', () => {
       cy.contains(/Ready to Dive Back In/i).should('exist');
     });
+
+    it('should display error messages for invalid input', () => {
+      cy.get('[data-testid="login-button"]').click();
+      cy.get('span').should('contain', 'All fields are required');
+      cy.get('input[type="email"]').type('invalid-email');
+      cy.get('input[type="password"]').type('password');
+      cy.get('[data-testid="login-button"]').click();
+      cy.get('span').should('contain', 'Please enter a valid email address');
+      cy.get('input[type="email"]').clear().type('valid@example.com');
+      cy.get('input[type="password"]').clear().type('wrongpassword');
+      cy.get('[data-testid="login-button"]').click();
+      cy.get('span').should('contain', 'Invalid email or password');
+    });
 });
+
+
 });
 
 
