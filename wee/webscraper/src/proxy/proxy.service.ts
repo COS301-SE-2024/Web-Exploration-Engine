bd5ee98168a9fef4302a513c8b751bb86fbe2a03
CaitMS <caitymsimon@gmail.com>
2024-08-14 20:07:08 +0200

    Revert "Merge pull request #329 from COS301-SE-2024/hotfix/rotate-proxies"
    This reverts commit 8762b782a9e07f122689bdd20ceb44604a18de5e, reversing
    changes made to 28503e643dead291177b575ee8f298c84b85be4e.

--- wee/webscraper/src/proxy/proxy.service.ts
+++ wee/webscraper/src/proxy/proxy.service.ts
@@ -2,25 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProxyService {
-  private readonly proxyPool = process.env.PROXIES;
-  private proxyArr: string[];
-  private readonly proxyPort = process.env.PROXY_PORT;
+  private readonly proxyPool = [
+    '13.245.162.149', // Techodyssey
+    '13.246.234.173', // Caitlin
+  ];
+  private readonly proxyPort = 3128;
   private readonly proxyUsername = process.env.PROXY_USERNAME;
   private readonly proxyPassword = process.env.PROXY_PASSWORD;
 
   constructor() {
-    if (!this.proxyPool) {
-      throw new Error('No PROXIES found')
-    }
     if (!this.proxyUsername || !this.proxyPassword) {
       throw new Error('PROXY_USERNAME or PROXY_PASSWORD not set');
     }
-    this.proxyArr = this.proxyPool.split(',');
-
   }
 
   getProxy() {
-    const proxy = this.proxyArr[Math.floor(Math.random() * this.proxyArr.length)];
+    const proxy = this.proxyPool[Math.floor(Math.random() * this.proxyPool.length)];
+    // return `http://${proxy}:${this.proxyPort}`;
     return `http://${proxy}:${this.proxyPort}`;
   }
 }
