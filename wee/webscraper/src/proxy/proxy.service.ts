9bd31145bd2a6985494edb8715ad660040110e81
CaitMS <caitymsimon@gmail.com>
2024-08-14 15:56:50 +0200

    Added proxy service tests and moved port to env variable

--- wee/webscraper/src/proxy/proxy.service.ts
+++ wee/webscraper/src/proxy/proxy.service.ts
@@ -2,19 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProxyService {
-  private readonly proxyPool = process.env.PROXIES.split(',');
-  private readonly proxyPort = 3128;
+  private readonly proxyPool = process.env.PROXIES;
+  private proxyArr: string[];
+  private readonly proxyPort = process.env.PROXY_PORT;
   private readonly proxyUsername = process.env.PROXY_USERNAME;
   private readonly proxyPassword = process.env.PROXY_PASSWORD;
 
   constructor() {
+    if (!this.proxyPool) {
+      throw new Error('No PROXIES found')
+    }
     if (!this.proxyUsername || !this.proxyPassword) {
       throw new Error('PROXY_USERNAME or PROXY_PASSWORD not set');
     }
+    this.proxyArr = this.proxyPool.split(',');
+
   }
 
   getProxy() {
-    const proxy = this.proxyPool[Math.floor(Math.random() * this.proxyPool.length)];
+    const proxy = this.proxyArr[Math.floor(Math.random() * this.proxyArr.length)];
     return `http://${proxy}:${this.proxyPort}`;
   }
 }
