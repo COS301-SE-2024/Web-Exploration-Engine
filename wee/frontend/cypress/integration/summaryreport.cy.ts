aa1f7dc75cbe29743468fda07e8a1c24f301d98a
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-08-16 15:17:27 +0200

    checking intial div values

--- wee/frontend/cypress/integration/summaryreport.cy.ts
+++ wee/frontend/cypress/integration/summaryreport.cy.ts
@@ -3,28 +3,130 @@ describe('summaryreport', () => {
     cy.visit('/summaryreport');
     cy.contains(/Summary Report/i).should('exist');
 
-       //Page Section : General Statistics
-       cy.get('[data-testid="visual-scraped-stats"]').should('exist');
-       cy.get('[data-testid="visual-scraped-stats"]').should('be.visible');
-       cy.get('[data-testid="visual-scraped-stats"]').screenshot('visual-scraped-stats');
- 
-       cy.get('[data-testid="visual-crawlable-stats"]').should('exist');
-       cy.get('[data-testid="visual-crawlable-stats"]').should('be.visible');
-       cy.get('[data-testid="visual-crawlable-stats"]').screenshot('visual-crawlable-stats');
- 
-       cy.get('[data-testid="visual-avg-scrape-stats"]').should('exist');
-       cy.get('[data-testid="visual-avg-scrape-stats"]').should('be.visible');
-       cy.get('[data-testid="visual-avg-scrape-stats"]').screenshot('visual-avg-scrape-stats');
- 
-       cy.get('[data-testid="visual-industry-classification"]').should('exist');
-       cy.get('[data-testid="visual-industry-classification"]').should('be.visible');
-       cy.get('[data-testid="visual-industry-classification"]').screenshot('visual-industry-classification');
- 
-       cy.get('[data-testid="visual-domain-match"]').should('exist');
-       cy.get('[data-testid="visual-domain-match"]').should('be.visible');
-       cy.get('[data-testid="visual-domain-match"]').screenshot('visual-domain-match');
- 
+    //Page Section : General Statistics
+    cy.get('[data-testid="visual-scraped-stats"]').should('exist');
+    cy.get('[data-testid="visual-scraped-stats"]').should('be.visible');
+    cy.get('[data-testid="visual-scraped-stats"]').screenshot(
+      'visual-scraped-stats'
+    );
+    cy.get('[data-testid="visual-scraped-stats"]')
+      .should('contain.text', 'Scraped', { matchCase: false })
+      .should('contain.text', 'Urls', { matchCase: false });
 
+    cy.get('[data-testid="visual-crawlable-stats"]').should('exist');
+    cy.get('[data-testid="visual-crawlable-stats"]').should('be.visible');
+    cy.get('[data-testid="visual-crawlable-stats"]').screenshot(
+      'visual-crawlable-stats'
+    );
+    cy.get('[data-testid="visual-crawlable-stats"]')
+      .should('contain.text', 'Crawlable', { matchCase: false })
+      .should('contain.text', 'Urls', { matchCase: false });
+
+    cy.get('[data-testid="visual-avg-scrape-stats"]').should('exist');
+    cy.get('[data-testid="visual-avg-scrape-stats"]').should('be.visible');
+    cy.get('[data-testid="visual-avg-scrape-stats"]').screenshot(
+      'visual-avg-scrape-stats'
+    );
+    cy.get('[data-testid="visual-avg-scrape-stats"]')
+      .should('contain.text', 'sec', { matchCase: false })
+      .should('contain.text', 'scrape time', { matchCase: false });
+
+    //Section : Industry Classification
+
+    //Industry Classification Div
+    cy.get('[data-testid="visual-industry-classification"]').should('exist');
+    cy.get('[data-testid="visual-industry-classification"]').should(
+      'be.visible'
+    );
+    cy.get('[data-testid="visual-industry-classification"]').screenshot(
+      'visual-industry-classification'
+    );
+    cy.get('[data-testid="visual-industry-classification"]').should(
+      'contain.text',
+      'Classification Distribution',
+      { matchCase: false }
+    );
+
+    //Weak Classifcation Div
+    cy.get('[data-testid="visual-weak-classifications"]').should('exist');
+    cy.get('[data-testid="visual-weak-classifications"]').should('be.visible');
+    cy.get('[data-testid="visual-weak-classifications"]').screenshot(
+      'visual-weak-classifications'
+    );
+    cy.get('[data-testid="visual-weak-classifications"]').should(
+      'contain.text',
+      'URLSCOREThere was no weak classifications',
+      { matchCase: false }
+    );
+
+    //Weak Classifcation Table
+    cy.get('[data-testid="table-weak-classifications"]').should('exist');
+    cy.get('[data-testid="table-weak-classifications"]').should('be.visible');
+    cy.get('[data-testid="table-weak-classifications"]').screenshot(
+      'visual-table-weak-classifications'
+    );
+    cy.get('[data-testid="table-weak-classifications"]').should(
+      'contain.text',
+      'URLSCOREThere was no weak classifications',
+      { matchCase: false }
+    );
+
+    cy.get('[data-testid="table-weak-classifications"]')
+      .find('th')
+      .first()
+      .should('contain.text', 'URL');
+
+    cy.get('[data-testid="table-weak-classifications"]')
+      .find('th')
+      .eq(1)
+      .should('contain.text', 'SCORE');
+
+    cy.get('[data-testid="table-weak-classifications"]')
+      .find('tbody')
+      .should('contain.text', 'no weak classifications', { matchCase: false });
+
+    //Section : Domain Match
+
+    //Domain Mismatch Information
+    cy.get('[data-testid="visual-domain-match"]').should('exist');
+    cy.get('[data-testid="visual-domain-match"]').should('be.visible');
+    cy.get('[data-testid="visual-domain-match"]').screenshot(
+      'visual-domain-match'
+    );
+    cy.get('[data-testid="visual-domain-match"]').should(
+      'contain.text',
+      'Match0%Domain mismatch informationURLCLASSIFICATION - METADOMAIN MATCHThere was no mismatch',
+      { matchCase: false }
+    );
+
+    //Domain Mismatch Table
+    cy.get('[data-testid="table-domain-match"]').should('exist');
+    cy.get('[data-testid="table-domain-match"]').should('be.visible');
+    cy.get('[data-testid="table-domain-match"]').screenshot(
+      'table-domain-match'
+    );
+    cy.get('[data-testid="table-domain-match"]').should(
+      'contain.text',
+      'URLCLASSIFICATION - METADOMAIN MATCHThere was no mismatch',
+      { matchCase: false }
+    );
+
+    cy.get('[data-testid="table-domain-match"]')
+      .find('th')
+      .eq(0)
+      .should('contain.text', 'URL');
+
+    cy.get('[data-testid="table-domain-match"]')
+      .find('th')
+      .eq(1)
+      .should('contain.text', 'CLASSIFICATION - META');
+
+    cy.get('[data-testid="table-domain-match"]')
+      .find('th')
+      .eq(2)
+      .should('contain.text', 'DOMAIN MATCH');
+
+    // Section : Classification Distribution
   });
 
   it('general layout should be consistent', () => {
