fc0f7ef699e723e211799a6633b4ad1def9c0362
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-08-16 15:18:50 +0200

    cypress waits, adding checks after mocking done

--- wee/frontend/cypress/integration/summaryreport.cy.ts
+++ wee/frontend/cypress/integration/summaryreport.cy.ts
@@ -288,17 +288,177 @@ describe('summaryreport', () => {
         ).as('mock_scraper_mockinsecure_status');
       });
 
+    //======================================================
+    // Cypress Waits
+    //======================================================
 
-    // Assert that the URL is the scraperesults URL
+    //Wait for github to finish
+    cy.wait('@mock_scraper_mockgithub_done');
+
+    //Wait for steers to finish
+    cy.wait('@mock_scraper_mocksteers_done');
+
+    //Wait for wimpy to finish
+    cy.wait('@mock_scraper_mockwimpy_done');
+
     cy.url().should('include', 'scraperesults');
+    cy.wait(9000);
 
-    //mocking now done - go to home page
-    /* cy.get('[data-testid="btn-report-summary"]').should('exist')
+    cy.get('[data-testid="btn-report-summary"]');
     cy.get('[data-testid="btn-report-summary"]').click();
-    cy.url().should('include', 'summary'); */
+    cy.url().should('include', 'summary');
 
+    //======================================================
+    // Checking Content
+    //======================================================
 
+    //cy.get('[data-test-id="your-div-id"]')
+    // Find the label within the selected div
+    //.find('label')
+    // Assert the text of the label
+    //.should('have.text', 'Expected Label Text');
+
+    //Page Section : General Statistics
+    cy.get('[data-testid="visual-scraped-stats"]').should('exist');
+    cy.get('[data-testid="visual-scraped-stats"]').should('be.visible');
+    cy.get('[data-testid="visual-scraped-stats"]').screenshot(
+      'visual-scraped-stats'
+    );
+    cy.get('[data-testid="visual-scraped-stats"]')
+      .should('contain.text', '3', { matchCase: false })
+      .should('contain.text', 'Scraped', { matchCase: false })
+      .should('contain.text', 'Urls', { matchCase: false });
+
+    cy.get('[data-testid="visual-crawlable-stats"]').should('exist');
+    cy.get('[data-testid="visual-crawlable-stats"]').should('be.visible');
+    cy.get('[data-testid="visual-crawlable-stats"]').screenshot(
+      'visual-crawlable-stats'
+    );
+    cy.get('[data-testid="visual-crawlable-stats"]')
+      .should('contain.text', '3', { matchCase: false })
+      .should('contain.text', 'Crawlable', { matchCase: false })
+      .should('contain.text', 'Urls', { matchCase: false });
+
+    cy.get('[data-testid="visual-avg-scrape-stats"]').should('exist');
+    cy.get('[data-testid="visual-avg-scrape-stats"]').should('be.visible');
+    cy.get('[data-testid="visual-avg-scrape-stats"]').screenshot(
+      'visual-avg-scrape-stats'
+    );
+    cy.get('[data-testid="visual-avg-scrape-stats"]')
+      .should('contain.text', '48.82', { matchCase: false })
+      .should('contain.text', 'sec', { matchCase: false })
+      .should('contain.text', 'scrape time', { matchCase: false });
+
+    //Section : Industry Classification
+
+    //Industry Classification Div
+    cy.get('[data-testid="visual-industry-classification"]').should('exist');
+    cy.get('[data-testid="visual-industry-classification"]').should(
+      'be.visible'
+    );
+    cy.get('[data-testid="visual-industry-classification"]').screenshot(
+      'visual-industry-classification'
+    );
+    cy.get('[data-testid="visual-industry-classification"]').should(
+      'contain.text',
+      'Classification Distribution',
+      { matchCase: false }
+    );
+
+    //Weak Classifcation Div
+    cy.get('[data-testid="visual-weak-classifications"]').should('exist');
+    cy.get('[data-testid="visual-weak-classifications"]').should('be.visible');
+    cy.get('[data-testid="visual-weak-classifications"]').screenshot(
+      'visual-weak-classifications'
+    );
+    cy.get('[data-testid="visual-weak-classifications"]').should(
+      'contain.text',
+      'Weak classificationsiURLSCOREhttps://github.com36.69%',
+      { matchCase: false }
+    );
+
+    //Weak Classifcation Table
+    cy.get('[data-testid="table-weak-classifications"]').should('exist');
+    cy.get('[data-testid="table-weak-classifications"]').should('be.visible');
+    cy.get('[data-testid="table-weak-classifications"]').screenshot(
+      'visual-table-weak-classifications'
+    );
+    cy.get('[data-testid="table-weak-classifications"]').should(
+      'contain.text',
+      'URLSCOREhttps://github.com36.69%',
+      { matchCase: false }
+    );
+
+    cy.get('[data-testid="table-weak-classifications"]')
+      .find('th')
+      .first()
+      .should('contain.text', 'URL');
+
+    cy.get('[data-testid="table-weak-classifications"]')
+      .find('th')
+      .eq(1)
+      .should('contain.text', 'SCORE');
+
+    cy.get('[data-testid="table-weak-classifications"]')
+      .find('tbody')
+      .should('contain.text', 'https://github.com36.69%', { matchCase: false });
+
+    //Section : Domain Match
+
+    //Domain Mismatch Information
+    cy.get('[data-testid="visual-domain-match"]').should('exist');
+    cy.get('[data-testid="visual-domain-match"]').should('be.visible');
+    cy.get('[data-testid="visual-domain-match"]').screenshot(
+      'visual-domain-match'
+    );
+    //cy.get('[data-testid="visual-domain-match"]')
+    //.should('contain.text','Match0%Domain mismatch informationURLCLASSIFICATION - METADOMAIN MATCHhttps://github.comConstructionMining and Mineralshttps://steers.co.zaRestaurantsFitness and Wellnesshttps://wimpy.co.zaRestaurantsFitness and Wellness',{ matchCase: false })
+
+    //Domain Mismatch Table
+    cy.get('[data-testid="table-domain-match"]').should('exist');
+    cy.get('[data-testid="table-domain-match"]').should('be.visible');
+    cy.get('[data-testid="table-domain-match"]').screenshot(
+      'table-domain-match'
+    );
+    //cy.get('[data-testid="table-domain-match"]')
+    //.should('contain.text','URLCLASSIFICATION - METADOMAIN MATCHhttps://github.comConstructionMining and Mineralshttps://steers.co.zaRestaurantsFitness and Wellnesshttps://wimpy.co.zaRestaurantsFitness and Wellness',{ matchCase: false })
+
+    cy.get('[data-testid="table-domain-match"]')
+      .find('th')
+      .eq(0)
+      .should('contain.text', 'URL');
+
+    cy.get('[data-testid="table-domain-match"]')
+      .find('th')
+      .eq(1)
+      .should('contain.text', 'CLASSIFICATION - META');
+
+    cy.get('[data-testid="table-domain-match"]')
+      .find('th')
+      .eq(2)
+      .should('contain.text', 'DOMAIN MATCH');
+
+    // Section : Classification Distribution
+    cy.get('[id="area-chart"]').should('exist').should('be.visible');
+
+    //Section : Save Export Button
+    cy.get('[id="btn-save-export"]')
+      .should('exist')
+      .should('be.visible')
+      .click();
+
+    // Save Button
+    cy.get('[data-testid="dropdown-item-save"]')
+      .should('be.visible')
+      .should(
+        'contain.text',
+        'Sign up or log in to save the report on our website'
+      )
+      .should('contain.text', 'Save');
+
+    cy.get('[data-testid="dropdown-item-download"]')
+      .should('be.visible')
+      .should('contain.text', 'Download the report to your device')
+      .should('contain.text', 'Download');
   });
-
-  
 });
