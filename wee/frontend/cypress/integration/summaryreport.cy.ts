a615f2f7fa32c4ffbb304b0bb8997f1cbb751308
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-08-16 15:18:02 +0200

    mocking added

--- wee/frontend/cypress/integration/summaryreport.cy.ts
+++ wee/frontend/cypress/integration/summaryreport.cy.ts
@@ -132,78 +132,162 @@ describe('summaryreport', () => {
   it('general layout should be consistent', () => {
     cy.testLayout('/summaryreport');
   });
-  
-  it('summary report for multiple urls - github, insecure', () => {
+
+  it('summary report for 3 urls - steers, wimpy, github', () => {
     cy.visit('/');
-    cy.get('[data-testid="scraping-textarea-home"]').type('https://github.com,https://www.hbo.com/insecure');
+    cy.get('[data-testid="scraping-textarea-home"]').type(
+      'https://mock.test.steers.co.za,https://mock.test.wimpy.co.za,https://mock.test.github.com'
+    );
     cy.get('[data-testid="btn-start-scraping"]').click();
-      
+
+    //======================================================
+    // GitHub Mock
+    //======================================================
+
+    //Full response
     cy.fixture('/pub-sub/github-done')
-      .as('mock_scraper_github')
-      .then((mock_scraper_github) => {
+      .as('mock_scraper_mockgithub_done')
+      .then((mock_scraper_mockgithub_done) => {
         cy.intercept(
           'GET',
-          'http://localhost:3002/api/scraper?url=https%3A%2F%2Fgithub.com',
-          mock_scraper_github
-        ).as('mock_scraper_github_done');
-      });
-      
-    cy.fixture('/pub-sub/github-done')
-      .as('mock_scraper_github')
-      .then((mock_scraper_github) => {
-        cy.intercept(
-          'GET',
-          'http://localhost:3002/api/scraper?url=https%3A%2F%2Fwww.hbo.com%2Finsecure',
-          mock_scraper_github
-        ).as('mock_scraper_insecure_done');
+          'http://localhost:3002/api/scraper?url=https%3A%2F%2Fmock.test.github.com', //mock.test.github.com
+          mock_scraper_mockgithub_done
+        ).as('mock_scraper_mockgithub_done');
       });
 
-    // Mock response to Pub Sub Polling
-    cy.fixture('/pub-sub/insecure-status')
-      .as('mock_scraper_insecure_status')
-      .then((mock_scraper_insecure_status) => {
-        cy.intercept(
-          'GET',
-          'http://localhost:3002/api/scraper/status/scrape/https%3A%2F%2Fwww.hbo.com%2Finsecure',
-          mock_scraper_insecure_status
-        ).as('mock_scraper_insecure_status');
-      });
-      
-    cy.fixture('/pub-sub/github-status')
-      .as('mock_scraper_github_status')
-      .then((mock_scraper_github_status) => {
-        cy.intercept(
-          'GET',
-          'http://localhost:3002/api/scraper/status/scrape/https%3A%2F%2Fgithub.com',
-          mock_scraper_github_status
-        ).as('mock_scraper_github_status');
-      });
-
-    cy.fixture('/pub-sub/insecure-waiting')
-      .as('mock_scraper_insecure_waiting')
-      .then((mock_scraper_insecure_waiting) => {
-        cy.intercept(
-          'GET',
-          'http://localhost:3002/api/scraper/status?type=scrape&url=https%3A%2F%2Fwww.hbo.com%2Finsecure',
-          mock_scraper_insecure_waiting
-        ).as('mock_scraper_insecure_waiting');
-      });
-      
+    //Pub Sub - Publish Event
     cy.fixture('/pub-sub/github-waiting')
-      .as('mock_scraper_github_waiting')
-      .then((mock_scraper_github_waiting) => {
+      .as('mock_scraper_mockgithub_waiting')
+      .then((mock_scraper_mockgithub_waiting) => {
         cy.intercept(
           'GET',
-          'http://localhost:3002/api/scraper/status?type=scrape&url=https%3A%2F%2Fgithub.com',
-          mock_scraper_github_waiting
-        ).as('mock_scraper_github_waiting');
+          'http://localhost:3002/api/scraper/status?type=scrape&url=https%3A%2F%2Fmock.test.github.com',
+          mock_scraper_mockgithub_waiting
+        ).as('mock_scraper_mockgithub_waiting');
       });
-      
-    cy.wait('@mock_scraper_github_done');
-    cy.wait('@mock_scraper_insecure_done');
 
-    //Wait 5 Seconds
-    //THen check if the text insecure and github exist on the page
+    //Pub Sub - Get Event Status
+    cy.fixture('/pub-sub/github-status')
+      .as('mock_scraper_mockgithub_status')
+      .then((mock_scraper_mockgithub_status) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper/status/scrape/https%3A%2F%2Fmock.test.github.com',
+          mock_scraper_mockgithub_status
+        ).as('mock_scraper_mockgithub_status');
+      });
+
+    //======================================================
+    // Steers Mock
+    //======================================================
+
+    //Full response
+    cy.fixture('/pub-sub/steers-done')
+      .as('mock_scraper_mocksteers_done')
+      .then((mock_scraper_mocksteers_done) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper?url=https%3A%2F%2Fmock.test.steers.co.za', //mock.test.steers.co.za
+          mock_scraper_mocksteers_done
+        ).as('mock_scraper_mocksteers_done');
+      });
+
+    //Pub Sub - Publish Event
+    cy.fixture('/pub-sub/steers-waiting')
+      .as('mock_scraper_mocksteers_waiting')
+      .then((mock_scraper_mocksteers_waiting) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper/status?type=scrape&url=https%3A%2F%2Fmock.test.steers.co.za',
+          mock_scraper_mocksteers_waiting
+        ).as('mock_scraper_mocksteers_waiting');
+      });
+
+    //Pub Sub - Get Event Status
+    cy.fixture('/pub-sub/steers-status')
+      .as('mock_scraper_mocksteers_status')
+      .then((mock_scraper_mocksteers_status) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper/status/scrape/https%3A%2F%2Fmock.test.steers.co.za',
+          mock_scraper_mocksteers_status
+        ).as('mock_scraper_mocksteers_status');
+      });
+
+    //======================================================
+    // Wimpy Mock
+    //======================================================
+
+    //Full response
+    cy.fixture('/pub-sub/wimpy-done')
+      .as('mock_scraper_mockwimpy_done')
+      .then((mock_scraper_mockwimpy_done) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper?url=https%3A%2F%2Fmock.test.wimpy.co.za', //mock.test.wimpy.co.za
+          mock_scraper_mockwimpy_done
+        ).as('mock_scraper_mockwimpy_done');
+      });
+
+    //Pub Sub - Publish Event
+    cy.fixture('/pub-sub/wimpy-waiting')
+      .as('mock_scraper_mockwimpy_waiting')
+      .then((mock_scraper_mockwimpy_waiting) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper/status?type=scrape&url=https%3A%2F%2Fmock.test.wimpy.co.za',
+          mock_scraper_mockwimpy_waiting
+        ).as('mock_scraper_mockwimpy_waiting');
+      });
+
+    //Pub Sub - Get Event Status
+    cy.fixture('/pub-sub/wimpy-status')
+      .as('mock_scraper_mockwimpy_status')
+      .then((mock_scraper_mockwimpy_status) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper/status/scrape/https%3A%2F%2Fmock.test.wimpy.co.za',
+          mock_scraper_mockwimpy_status
+        ).as('mock_scraper_mockwimpy_status');
+      });
+
+    //======================================================
+    // Insecure Mock
+    //======================================================
+
+    //Full response
+    cy.fixture('/pub-sub/insecure-done')
+      .as('mock_scraper_mockinsecure_done')
+      .then((mock_scraper_mockinsecure_done) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper?url=https%3A%2F%2Fmock.test.insecure.co.za', //mock.test.insecure.co.za
+          mock_scraper_mockinsecure_done
+        ).as('mock_scraper_mockinsecure_done');
+      });
+
+    //Pub Sub - Publish Event
+    cy.fixture('/pub-sub/insecure-waiting')
+      .as('mock_scraper_mockinsecure_waiting')
+      .then((mock_scraper_mockinsecure_waiting) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper/status?type=scrape&url=https%3A%2F%2Fmock.test.insecure.co.za',
+          mock_scraper_mockinsecure_waiting
+        ).as('mock_scraper_mockinsecure_waiting');
+      });
+
+    //Pub Sub - Get Event Status
+    cy.fixture('/pub-sub/insecure-status')
+      .as('mock_scraper_mockinsecure_status')
+      .then((mock_scraper_mockinsecure_status) => {
+        cy.intercept(
+          'GET',
+          'http://localhost:3002/api/scraper/status/scrape/https%3A%2F%2Fmock.test.insecure.co.za',
+          mock_scraper_mockinsecure_status
+        ).as('mock_scraper_mockinsecure_status');
+      });
+
 
     // Assert that the URL is the scraperesults URL
     cy.url().should('include', 'scraperesults');
