695b0008bb6152ed5ddb3eeec679b9eb120940e4
CaitMS <caitymsimon@gmail.com>
2024-08-24 23:07:52 +0200

    move logs into wee folder

--- wee/webscraper/logging/webscraperlogger.ts
+++ wee/webscraper/logging/webscraperlogger.ts
@@ -3,7 +3,7 @@ import 'winston-daily-rotate-file';
 
 const getLogger = (fileName = 'application') => {
   const fileLogTransport = new transports.DailyRotateFile({
-    filename: `../logs/${fileName}-%DATE%.log`,
+    filename: `./logs/${fileName}-%DATE%.log`,
     datePattern: 'YYYY-MM-DD',
     zippedArchive: true,
     maxSize: '20m',
