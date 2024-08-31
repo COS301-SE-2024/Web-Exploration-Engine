389a40e8db73abbb2fce036089e8edeff5d88a4d
MignonErasmus <u22492586@tuks.co.za>
2024-08-31 15:14:27 +0200

    google auth working now

--- wee/frontend/src/app/services/AuthService.ts
+++ wee/frontend/src/app/services/AuthService.ts
@@ -75,26 +75,3 @@ export async function signUp(req: SignUpRequest) {
     emailVerified: data?.user?.email_confirmed_at ? true : false,
   }
 }
-
-export async function googleLogin() {
-  const supabase = createClient();
-  const { error } = await supabase.auth.signInWithOAuth({
-    provider: 'google',
-  });
-  if (error) {
-    console.error('Error logging in with Google:', error.message);
-    return {
-      code: error.code,
-      message: error.message,
-    }
-  }
-
-  // get user data
-  const { data: { user } } = await supabase.auth.getUser();
-  const userName = user?.user_metadata?.first_name || user?.user_metadata?.name || user?.user_metadata?.fullname || user?.email || '';
-  return { 
-    uuid: user?.id,
-    emailVerified: user?.email_confirmed_at ? true : false,
-    name: userName ,
-  }
-}
---
+++ wee/frontend/src/app/services/OAuthService.ts
@@ -0,0 +1,25 @@
+import { createClient } from "../utils/supabase/client";
+
+export async function googleLogin() {
+    const supabase = createClient();
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    });
+  
+    if (error) {
+      console.error('Error logging in with Google:', error.message);
+      return {
+        code: error.code,
+        message: error.message,
+      }
+    }
+  
+    // get user data
+    const { data: { user } } = await supabase.auth.getUser();
+    const userName = user?.user_metadata?.first_name || user?.user_metadata?.name || user?.user_metadata?.fullname || user?.email || '';
+    return { 
+      uuid: user?.id,
+      emailVerified: user?.email_confirmed_at ? true : false,
+      name: userName ,
+    }
+  }
