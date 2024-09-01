c7f6746a50ccfad405c2dd422b8bfd48b485f00c
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2024-09-01 02:03:15 +0200

    extract logged content as json

--- wee/webscraper/logging/webscraperlogger.ts
+++ wee/webscraper/logging/webscraperlogger.ts
@@ -4,20 +4,29 @@ import path from 'path';
 
 // Custom format to handle message and service extraction
 const customFormat = winston.format((info) => {
-  console.log(info)
-  console.log("==========================================")
+  console.log(info);
+  console.log('==========================================');
 
   // Check if the message is passed as a string and the second argument is a string
-  if (typeof info.message === 'string' && Array.isArray(info[Symbol.for('splat')])) {
+  if (
+    typeof info.message === 'string' &&
+    Array.isArray(info[Symbol.for('splat')])
+  ) {
     const splat = info[Symbol.for('splat')];
     if (splat.length > 0 && typeof splat[0] === 'string') {
       info.service = splat[0]; // Assign the second argument as the service
+      /*
+      
+      if info.service has text interceptor, create 
+      info.performance = splat[2]
+      info.meta = splat
+      */
       info.message = splat[1]; // Assign the second argument as the service
-      info.meta = splat;  // Keep the original message
+      info.meta = splat; // Keep the original message
     }
   }
- // console.log(info.message) == the app is starting x 2
- console.log(info)
+  // console.log(info.message) == the app is starting x 2
+  console.log(info);
   //looop for all elements in variables
   return info;
 })();
@@ -31,33 +40,21 @@ const getLogger = (fileName = 'application') => {
     maxFiles: '100d',
   });
 
-
   const winston = require('winston');
 
-
-// const logger = winston.createLogger({
-//   level: process.env.LOG_LEVEL || 'info',
-//   defaultMeta: {
-//     service: 'admin-service',
-//   },
-//   format: winston.format.json(),
-//   server: winston.format.json(),
-//   transports: [new winston.transports.Console()],
-// });
-
-// Create the logger instance
-const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp(), // Adds a timestamp to the logs
-    customFormat,
-    winston.format.json() // Output the logs in JSON format
-  ),
-  transports: [
-    new winston.transports.File({ filename: path.join(__dirname, 'logs.json') }), // Save logs to a file
-  ],
-});
-
-  
+  // Create the logger instance
+  const logger = winston.createLogger({
+    format: winston.format.combine(
+      winston.format.timestamp(), // Adds a timestamp to the logs
+      customFormat,
+      winston.format.json() // Output the logs in JSON format
+    ),
+    transports: [
+      new winston.transports.File({
+        filename: path.join(__dirname, 'logs.json'),
+      }), // Save logs to a file
+    ],
+  });
 
   logger.add(fileLogTransport);
 
