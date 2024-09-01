c4c4187ba49ffb39ae46abc7ff8bb35bf01b2a27
MignonErasmus <u22492586@tuks.co.za>
2024-09-01 15:50:09 +0200

    added keyword addition and removal functionality with some error handling

--- wee/frontend/src/app/(pages)/scheduledscrape/page.tsx
+++ wee/frontend/src/app/(pages)/scheduledscrape/page.tsx
@@ -3,10 +3,44 @@ import React from 'react';
 import { Button, Modal, ModalHeader, ModalContent, ModalBody, useDisclosure, ModalFooter, SelectItem, DatePicker } from '@nextui-org/react';
 import WEEInput from '../../components/Util/Input';
 import WEESelect from '../../components/Util/Select';
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiTrash } from "react-icons/fi";
+import { MdErrorOutline } from "react-icons/md";
+import { now, getLocalTimeZone } from "@internationalized/date";
 
 export default function ScheduledScrape() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [keyword, setKeyword] = React.useState('');
+  const [keywordList, setKeywordList] = React.useState(['keywordOne', 'keyword phrase two']);
+  const [modalError, setModalError] = React.useState('');
+
+  // Add keyword to keyword list
+  const handleAddKeyword = () => {
+    // show error message if the keyword list exceeds a length of 3
+    if (keywordList.length == 3 && keyword.trim() !== '') {
+      setModalError("Max of 3 keywords can be tracked");
+
+      const timer = setTimeout(() => {
+        setModalError('');
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+
+    if (keyword.trim() !== '') {
+      setKeywordList([...keywordList, keyword.trim()]); // Add the keyword to the list
+      setKeyword(''); // Clear the input field
+    }
+  };
+
+  // Remove keyword from keyword list
+  const handleDeleteKeyword = (index: number) => {
+    const updatedKeywords = keywordList.filter((_, i) => i !== index);
+    setKeywordList(updatedKeywords); // Remove the keyword from the list
+  };
+
+  const handleScheduledScrapeTaskAdd = () => {
+    alert('Here');
+  };
 
   return (
     <>
@@ -32,12 +66,14 @@ export default function ScheduledScrape() {
             <>
               <ModalHeader className="flex flex-col gap-1 text-jungleGreen-800 dark:text-dark-primaryTextColor">Add Scheduled Scraping Task</ModalHeader>
               <ModalBody>
+                {/* Url to schedule scrape */}
                 <WEEInput
                   type="text"
                   label="Url to scrape"
                   className="sm:w-4/5 md:w-full lg:w-4/5"
                 />
 
+                {/* Frequency */}
                 <WEESelect
                   label="Scraping Frequency"
                   data-testid="frequency-select"
@@ -48,17 +84,63 @@ export default function ScheduledScrape() {
                   <SelectItem key='3' textValue='Monthly'>Monthly</SelectItem>
                 </WEESelect>
 
+                {/* Start scraping */}
                 <DatePicker
-                  label="Event Date"
-                  variant="bordered"
+                  label="Start scraping date and time"
+                  // variant="bordered"
                   hideTimeZone
                   showMonthAndYearPickers
+                  defaultValue={now(getLocalTimeZone())}
                 />
+
+                {/* Add keyword or phrase */}
+                <div className='flex'>
+                  <WEEInput
+                    type="text"
+                    label="Keyword or phrase"
+                    className='pr-2'
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                  />
+                  <Button
+                    isIconOnly
+                    onClick={handleAddKeyword}
+                    className='my-auto font-poppins-semibold text-md md:text-lg bg-jungleGreen-700 text-dark-primaryTextColor dark:bg-jungleGreen-400 dark:text-primaryTextColor'
+                  >
+                    <FiPlus />
+                  </Button>
+                </div>
+
+                {/* List of added keywords or phrases */}
+                <div>
+                  {keywordList.map((kw, index) => (
+                    <div key={index} className='flex justify-between'>
+                      <span className='hover:cursor-default'>{kw}</span>
+                      <span
+                        className='my-auto text-red-600 hover:cursor-pointer'
+                        onClick={() => handleDeleteKeyword(index)}
+                      >
+                        <FiTrash />
+                      </span>
+                    </div>
+                  ))}
+                </div>
+
+                {modalError ? (
+                  <span className="mt-4 mb-2 p-2 text-white bg-red-600 rounded-lg transition-opacity duration-300 ease-in-out flex justify-center align-middle">
+                    <MdErrorOutline className="m-auto mx-1" />
+                    <p data-testid="keyword-error">{modalError}</p>
+                  </span>
+                ) : (
+                  <></>
+                )}
+
               </ModalBody>
               <ModalFooter>
                 <Button
                   className='font-poppins-semibold text-md md:text-lg bg-jungleGreen-700 text-dark-primaryTextColor dark:bg-jungleGreen-400 dark:text-primaryTextColor'
                   onPress={onClose}
+                  onClick={handleScheduledScrapeTaskAdd}
                 >
                   Add Task
                 </Button>
