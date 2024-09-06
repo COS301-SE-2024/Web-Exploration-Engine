e8e328f56422bdd67a7f44f2d48151ceb72e4339
MignonErasmus <u22492586@tuks.co.za>
2024-09-06 16:51:36 +0200

    added stacked column chart for the reviews (stars)

--- wee/frontend/src/app/(pages)/dashboard/page.tsx
+++ wee/frontend/src/app/(pages)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { InfoPopOver } from '../../components/InfoPopOver';
 import { LineChart } from '../../components/Graphs/LineChart';
 import { AreaChart } from '../../components/Graphs/AreaChart';
 import { ColumnChartNPS } from '../../components/Graphs/ColumnChart';
+import { StackedColumnChart } from '../../components/Graphs/StackedColumnChart';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import { Button } from '@nextui-org/react';
@@ -94,7 +95,7 @@ function DashboardPage() {
 
 			<div>
 				<h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
-					Reviews
+					NPS Reviews
 					<InfoPopOver
 						data-testid="popup-nps-reviews"
 						heading="News Sentiment"
@@ -106,6 +107,41 @@ function DashboardPage() {
 					<ColumnChartNPS
 						dataLabel={['10 Feb', '10 Mar', '10 Apr', '10 May', '10 Jun', '10 Jul', '10 Aug', '10 Sept']}
 						dataSeries={[10, -44, 49, 50, 90, 0, 10, 45]}
+					/>
+				</div>
+				<h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
+					Reviews - Stars
+					<InfoPopOver
+						data-testid="popup-nps-reviews"
+						heading="News Sentiment"
+						content="Add description here"
+						placement="right-end"
+					/>
+				</h3>
+				<div className='bg-zinc-200 dark:bg-zinc-700 p-4 rounded-xl text-center'>
+					<StackedColumnChart 
+						dataLabel={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']} 
+						dataSeries={[        
+						{
+							name: '1 Star',
+							data: [32, 15, 14, 20, 22, 48]
+						},
+						{
+							name: '2 Stars',
+							data: [25, 30, 48, 35, 33, 30]
+						},
+						{
+							name: '3 Stars',
+							data: [18, 22, 20, 25, 28, 30]
+						},
+						{
+							name: '4 Stars',
+							data: [10, 45, 18, 22, 25, 28]
+						},
+						{
+							name: '5 Stars',
+							data: [50, 12, 15, 18, 22, 25]
+						}]}
 					/>
 				</div>
 			</div>
---
+++ wee/frontend/src/app/components/Graphs/StackedColumnChart.tsx
@@ -0,0 +1,104 @@
+'use client'
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { ApexOptions } from "apexcharts";
+import IChart from "../../models/ChartModel";
+import { ChartColours, DarkChartColours } from "./colours";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+export function StackedColumnChart({ dataLabel, dataSeries}: IChart) {
+    const { theme } = useTheme();
+
+    const [options, setOptions] = useState<ApexOptions>({
+        chart: {
+            id: 'apexchart-bar',
+            fontFamily: "'Poppins', sans-serif",
+            background: 'transparent',
+            height: 100, // or any other fixed height
+            width: '100%',
+            type: 'bar',
+            stacked: true,
+            toolbar: {
+                tools: {
+                    zoom: false,
+                    zoomin: true,
+                    zoomout: true,
+                    pan: false,
+                    reset: false,
+                    download: false,
+                }
+            }
+        },
+        colors: theme === 'light' ? ChartColours : DarkChartColours,
+        plotOptions: {
+            bar: {
+                horizontal: false // determines whether it is a horizontal(true) or vertical(false) chart
+            }
+        },
+        theme: {
+            mode: theme === 'dark' ? 'dark' : 'light'
+        },
+        xaxis: {
+            categories: dataLabel,
+            axisBorder: {
+                show: true,
+                color: theme === 'dark' ? '#D7D7D7' : '#BBBBBB',
+            },
+        },
+        yaxis: {
+            axisBorder: {
+                show: true,
+                color: theme === 'dark' ? '#D7D7D7' : '#BBBBBB',
+            }
+        },
+        grid: {
+            borderColor: theme === 'dark' ? '#D7D7D7' : '#BBBBBB',
+        },
+    });
+    
+    const series = dataSeries;    
+
+    useEffect(() => {
+        setOptions(prevOptions => ({
+            ...prevOptions,
+            colors: theme === 'light' ? ChartColours : DarkChartColours,
+            theme: {
+                mode: theme === 'dark' ? 'dark' : 'light'
+            },
+            xaxis: {
+                categories: dataLabel,
+                axisBorder: {
+                    show: true,
+                    color: theme === 'dark' ? '#D7D7D7' : '#BBBBBB',
+                }
+            },
+            yaxis: {
+                axisBorder: {
+                    show: true,
+                    color: theme === 'dark' ? '#D7D7D7' : '#BBBBBB',
+                }
+            },
+            grid: {
+                borderColor: theme === 'dark' ? '#D7D7D7' : '#BBBBBB',
+            }
+        }));
+    }, [theme, dataLabel]);
+
+    return (
+        <div className="app">
+            <div className="row">
+                <div className="mixed-chart">
+                    <Chart
+                        options={options}
+                        series={series}
+                        type="bar"
+                        height={280}
+                        width="100%"
+                    />
+                </div>
+            </div>
+        </div>        
+    );   
+}
