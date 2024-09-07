7530883a3fa97bb3006165e493116153fb6335a8
MignonErasmus <u22492586@tuks.co.za>
2024-09-07 07:21:56 +0200

    updated dashboard layout

--- wee/frontend/src/app/(pages)/dashboard/page.tsx
+++ wee/frontend/src/app/(pages)/dashboard/page.tsx
@@ -44,7 +44,7 @@ function DashboardPage() {
 
 			{/* Keyword tracking */}
 			<div>
-				<h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
+				<h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">
 					SEO Keyword Analysis
 					<InfoPopOver
 						data-testid="popup-seo-keyword-analaysis"
@@ -71,14 +71,25 @@ function DashboardPage() {
 						<h3 className="font-poppins-semibold text-md text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
 							Readerswarehouse south africa
 						</h3>
-						<LineChart areaCategories={['10 Jan', '10 Feb', '10 Mar', '10 Apr']} areaSeries={[{ name: 'Ranking', data: [4, 7, 8, 7] }]} />
+						<LineChart
+							areaCategories={[
+								'10 Jan 24', '10 Feb 24', '10 Mar 24', '10 Apr 24', '10 May 24', '10 Jun 24', '10 Jul 24',
+								'10 Aug 24', '10 Sep 24', '10 Oct 24', '10 Nov 24', '10 Dec 24', '10 Jan 25', '10 Feb 25', '10 Mar 25'
+							]}
+							areaSeries={[
+								{
+									name: 'Ranking',
+									data: [4, 7, 8, 7, 9, 6, 8, 7, 5, 8, 6, 9, 7, 8, 6]
+								}
+							]}
+						/>
 					</div>
 				</div>
 			</div>
 
-			{/* Keyword tracking */}
+			{/* News sentiment */}
 			<div>
-				<h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
+				<h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">
 					News Sentiment
 					<InfoPopOver
 						data-testid="popup-news-sentiment"
@@ -93,9 +104,10 @@ function DashboardPage() {
 				</div>
 			</div>
 
+			{/* Reviews */}
 			<div>
-				<h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
-					NPS Reviews
+				<h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100">
+					Reviews
 					<InfoPopOver
 						data-testid="popup-nps-reviews"
 						heading="News Sentiment"
@@ -103,49 +115,67 @@ function DashboardPage() {
 						placement="right-end"
 					/>
 				</h3>
-				<div className='bg-zinc-200 dark:bg-zinc-700 p-4 rounded-xl text-center'>
-					<ColumnChartNPS
-						dataLabel={['10 Feb', '10 Mar', '10 Apr', '10 May', '10 Jun', '10 Jul', '10 Aug', '10 Sept']}
-						dataSeries={[10, -44, 49, 50, 90, 0, 10, 45]}
+
+				<div className='bg-zinc-200 dark:bg-zinc-700 p-4 rounded-xl text-center mb-[1rem]'>
+					<h3 className="font-poppins-semibold text-md text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
+						Ratings
+					</h3>
+					<StackedColumnChart
+						dataLabel={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']}
+						dataSeries={[
+							{
+								name: '1 Star',
+								data: [32, 15, 14, 20, 22, 48]
+							},
+							{
+								name: '2 Stars',
+								data: [25, 30, 48, 35, 33, 30]
+							},
+							{
+								name: '3 Stars',
+								data: [18, 22, 20, 25, 28, 30]
+							},
+							{
+								name: '4 Stars',
+								data: [10, 45, 18, 22, 25, 28]
+							},
+							{
+								name: '5 Stars',
+								data: [50, 12, 15, 18, 22, 25]
+							}]}
 					/>
 				</div>
-				<h3 className="font-poppins-semibold text-lg text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
-					Reviews - Stars
-					<InfoPopOver
-						data-testid="popup-nps-reviews"
-						heading="News Sentiment"
-						content="Add description here"
-						placement="right-end"
-					/>
-				</h3>
-				<div className='bg-zinc-200 dark:bg-zinc-700 p-4 rounded-xl text-center'>
-					<StackedColumnChart 
-						dataLabel={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']} 
-						dataSeries={[        
-						{
-							name: '1 Star',
-							data: [32, 15, 14, 20, 22, 48]
-						},
-						{
-							name: '2 Stars',
-							data: [25, 30, 48, 35, 33, 30]
-						},
-						{
-							name: '3 Stars',
-							data: [18, 22, 20, 25, 28, 30]
-						},
-						{
-							name: '4 Stars',
-							data: [10, 45, 18, 22, 25, 28]
-						},
-						{
-							name: '5 Stars',
-							data: [50, 12, 15, 18, 22, 25]
-						}]}
-					/>
+
+				<div className='gap-4 grid md:grid-cols-2 lg:grid-cols-2'>
+					<div className='bg-zinc-200 dark:bg-zinc-700 p-4 rounded-xl text-center'>
+						<h3 className="font-poppins-semibold text-md text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
+							Index
+							<InfoPopOver
+								data-testid="popup-nps-reviews"
+								heading="News Sentiment"
+								content="Add description here"
+								placement="right-end"
+							/>
+						</h3>
+						<LineChart areaCategories={['10 Jan', '10 Feb', '10 Mar', '10 Apr', '10 May', '10 Jun', '10 Jul']} areaSeries={[{ name: 'Ranking', data: [8, 10, 18, 4, 7, 5, 3] }]} />
+					</div>
+					<div className='bg-zinc-200 dark:bg-zinc-700 p-4 rounded-xl text-center'>
+						<h3 className="font-poppins-semibold text-md text-jungleGreen-700 dark:text-jungleGreen-100 pb-2">
+							NPS Reviews
+							<InfoPopOver
+								data-testid="popup-nps-reviews"
+								heading="News Sentiment"
+								content="Add description here"
+								placement="right-end"
+							/>
+						</h3>
+						<ColumnChartNPS
+							dataLabel={['10 Feb', '10 Mar', '10 Apr', '10 May', '10 Jun', '10 Jul', '10 Aug', '10 Sept']}
+							dataSeries={[10, -44, 49, 50, 90, 0, 10, 45]}
+						/>
+					</div>
 				</div>
 			</div>
-
 		</div>
 	)
 }
