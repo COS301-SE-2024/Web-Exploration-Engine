fb23f09c4a18f35f2313661445e359015698ea8e
katnicole14 <u22543946@tuks.co.za>
2024-09-19 16:13:31 +0200

    Update savedReport.cy.ts

--- wee/e2e/frontend-e2e/src/e2e/savedReport.cy.ts
+++ wee/e2e/frontend-e2e/src/e2e/savedReport.cy.ts
@@ -1,6 +1,6 @@
 describe('saved reports page', () => {
 
-  it('summaries page content should be present', () => {
+  it('Saving and viewing saved reports', () => {
     cy.visit('/');
 
     //login first
@@ -80,16 +80,62 @@ describe('saved reports page', () => {
       });
 
 
+      //go back
+      cy.contains('button', 'Back').click();
+
      //saved summaries
 
-     cy.get('[data-testid="tab-summaries"]')
-    .click()
-    .wait(6000); // Waits for 2000 milliseconds (2 seconds) after clicking
+     cy.get('[data-testid="tab-summaries"]', { timeout: 60000 })
+    .click();
+     // Waits for 2000 milliseconds (2 seconds) after clicking
 
 
       cy.get('table').should('contain', 'myFirstSummary');
       cy.get('table').should('contain', '2024-09-19T07:07:55.772+00:00');
 
+
+      cy.get(`[data-testid="btnView0"]`)
+      .should('be.visible')  // Ensure the button is visible
+      .click();
+
+cy.contains('Saved Reports').should('be.visible');
+cy.contains('Scheduled Tasks').should('be.visible');
+cy.contains('myFirstSummary').should('be.visible');
+cy.contains('Thu Sep 19 2024').should('be.visible');
+cy.contains('General stats').should('be.visible');
+cy.contains('2 Urls').should('be.visible');
+cy.contains('Scraped').should('be.visible');
+cy.contains('2 Urls').should('be.visible');
+cy.contains('Crawlable').should('be.visible');
+// cy.contains('0.21 sec').should('be.visible');
+cy.contains('Avg scrape time').should('be.visible');
+cy.contains('Industry classification').should('be.visible');
+cy.contains('Classification Distribution').should('be.visible');
+cy.contains('Weak classifications').should('be.visible');
+cy.contains('https://wee-test-site-2.netlify.app/').should('be.visible');
+cy.contains('42.80%').should('be.visible');
+cy.contains('Domain match').should('be.visible');
+cy.contains('Domain mismatch information').should('be.visible');
+cy.contains('https://wee-test-site-1.netlify.app/').should('be.visible');
+cy.contains('Health Care').should('be.visible');
+cy.contains('Telecommunications').should('be.visible');
+cy.contains('Aerospace').should('be.visible');
+cy.contains('Website status').should('be.visible');
+cy.contains('Parked sites').should('be.visible');
+cy.contains('There were no parked websites').should('be.visible');
+
+cy.get('button[data-testid="dropdown-export"]')
+  .should('be.visible')
+  .click();  // Click the export button
+
+// After clicking the export button, click the option to download the report
+cy.contains('Download the report to your device')
+  .should('be.visible')
+  .click();  // Click to download the report
+
+  cy.contains('Log Out')
+  .should('be.visible')
+  .click();
 
 
   });
