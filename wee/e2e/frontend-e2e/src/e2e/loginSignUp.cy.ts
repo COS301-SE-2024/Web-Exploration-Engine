5924d864b949a0be81e93263cf16a40a7e120231
katnicole14 <u22543946@tuks.co.za>
2024-09-23 09:54:59 +0200

    loginsign up

--- wee/e2e/frontend-e2e/src/e2e/loginSignUp.cy.ts
+++ wee/e2e/frontend-e2e/src/e2e/loginSignUp.cy.ts
@@ -4,9 +4,7 @@ describe('frontend-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Check the greeting message
+      
     getGreeting().contains(/The Web Exploration Engine/);
   });
 
@@ -28,11 +26,12 @@ describe('frontend-e2e', () => {
       cy.visit('/login');
 
       // Fill out the login form
-      cy.get('input').eq(0).type('testuser@example.com'); // Email
+      cy.get('input').eq(0).type('testusere2e@example.com'); // Email
       cy.get('input').eq(1).type('password123');          // Password
 
       // Click submit button
       cy.get('button[data-testid="login-button"]').click();
+
     });
   });
   });
