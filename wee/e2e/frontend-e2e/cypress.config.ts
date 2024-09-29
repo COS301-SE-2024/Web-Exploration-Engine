19c91561a607369790adb594cf14946267df79f6
katnicole14 <u22543946@tuks.co.za>
2024-09-29 12:32:12 +0200

    disable support file

--- wee/e2e/frontend-e2e/cypress.config.ts
+++ wee/e2e/frontend-e2e/cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'https://capstone-wee.dns.net.za',
     defaultCommandTimeout: 6000,
+    supportFile: false, // Disable the support file
   },
   retries: {
     experimentalStrategy: 'detect-flake-but-always-fail',
