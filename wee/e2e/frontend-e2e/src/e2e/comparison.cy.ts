ed5a4252a9fe51678c38ebe4b379464a3a6e7380
katnicole14 <u22543946@tuks.co.za>
2024-09-29 12:20:23 +0200

    more tests

--- wee/e2e/frontend-e2e/src/e2e/comparison.cy.ts
+++ wee/e2e/frontend-e2e/src/e2e/comparison.cy.ts
@@ -4,27 +4,27 @@ describe('Comparison Page', () => {
     cy.visit('/');
   });
 
-  // it('should have all elements on the comparison page', () => {
-  //   cy.visit('/comparison');
+  it('should have all elements on the comparison page', () => {
+    cy.visit('/comparison');
 
-  //   // Check all components exist
-  //   cy.get('[data-testid="website1-select"]').should('exist');
-  //   cy.get('[data-testid="website2-select"]').should('exist');
+    // Check all components exist
+    cy.get('[data-testid="website1-select"]').should('exist');
+    cy.get('[data-testid="website2-select"]').should('exist');
 
-  //   // Page Section: Domain Overview
-  //   cy.get('[data-testid="sect-website-status"]').should('exist');
-  //   cy.get('[data-testid="sect-industry-classification"]').should('exist');
-  //   cy.get('[data-testid="sect-domain-match"]').should('exist');
+    // Page Section: Domain Overview
+    cy.get('[data-testid="sect-website-status"]').should('exist');
+    cy.get('[data-testid="sect-industry-classification"]').should('exist');
+    cy.get('[data-testid="sect-domain-match"]').should('exist');
 
-  //   // Page Section: On-page SEO analysis
-  //   cy.get('[data-testid="sect-unique-content"]').should('exist');
-  //   cy.get('[data-testid="sect-images"]').should('exist');
+    // Page Section: On-page SEO analysis
+    cy.get('[data-testid="sect-unique-content"]').should('exist');
+    cy.get('[data-testid="sect-images"]').should('exist');
 
-  //   // Page Section: Technical SEO analysis
-  //   cy.get('[data-testid="sect-lighthouse"]').should('exist');
-  //   cy.get('[data-testid="sect-mobile-friendly"]').should('exist');
-  //   cy.get('[data-testid="sect-site-speed"]').should('exist');
-  // });
+    // Page Section: Technical SEO analysis
+    cy.get('[data-testid="sect-lighthouse"]').should('exist');
+    cy.get('[data-testid="sect-mobile-friendly"]').should('exist');
+    cy.get('[data-testid="sect-site-speed"]').should('exist');
+  });
 
   it('should compare results of 2 URLs - wee-test-site-1 and wee-test-site-2', () => {
     // Define the URLs to scrape
@@ -53,7 +53,7 @@ describe('Comparison Page', () => {
       if ($body.find('[data-testid="loading-indicator"]').length === 0) {
 
     // Navigate to the comparison page
-    cy.get('[data-testid="btn-comparison-summary"]').should('exist').click();
+    // cy.get('[data-testid="btn-comparison-summary"]').should('exist').click();
 
 
     // // Select first website to compare
